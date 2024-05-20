9cd4434749d3d4748bfc660c2d8fbb092b32b898
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-05-20 05:46:56 +0300

    feat(server): define lessons and slots tables schema

--- server/migrations/1716146586880_setup.js
+++ server/migrations/1716146586880_setup.js
@@ -10,14 +10,23 @@ exports.shorthands = undefined;
  */
 exports.up = (pgm) => {
   pgm.createType("user_type", ["super_admin", "reg_admin", "tutor", "student"]);
+
+  pgm.createType("repeat_type", [
+    "no_repeat",
+    "daily",
+    "every_week",
+    "every_month",
+  ]);
+
   pgm.createTable("users", {
-    id: "id",
+    id: { type: "serial", primaryKey: true, notNull: true },
     email: { type: "varchar(1000)", notNull: true, unique: true },
     password: { type: "varchar(100)", notNull: true },
     name: { type: "varchar(100)", notNull: true },
-    avatar: { type: "varchar(255)" },
+    avatar: { type: "varchar(255)", default: null },
     type: { type: "user_type", notNull: true },
   });
+
   pgm.createTable("tutors", {
     id: {
       type: "serial",
@@ -25,16 +34,46 @@ exports.up = (pgm) => {
       primaryKey: true,
       references: "users(id)",
     },
-    bio: { type: "varchar(1000)" },
-    about: { type: "text" },
-    video: { type: "varchar(255)" },
+    bio: { type: "varchar(1000)", default: null },
+    about: { type: "text", default: null },
+    video: { type: "varchar(255)", default: null },
     authorized_zoom_app: { type: "boolean", notNull: true, default: false },
-    zoom_refresh_token: { type: "varchar(1000)" },
-    aquired_refresh_token_at: { type: "timestamp" },
+    zoom_refresh_token: { type: "varchar(1000)", default: null },
+    aquired_refresh_token_at: { type: "timestamp", default: null },
     created_at: { type: "timestamp", notNull: true },
     updated_at: { type: "timestamp", notNull: true },
   });
 
+  pgm.createTable("slots", {
+    id: { type: "serial", primaryKey: true, notNull: true },
+    tutor_id: { type: "serial", notNull: true, references: "users(id)" },
+    title: { type: "varchar(255)", notNull: true },
+    description: { type: "text", default: null },
+    weekday: { type: "smallint", notNull: true },
+    start_time: { type: "time", notNull: true },
+    end_time: { type: "time", notNull: true },
+    start_date: { type: "date", notNull: true },
+    end_date: { type: "date", default: null },
+    repeat_type: { type: "repeat_type", notNull: true, default: "no_repeat" },
+    created_at: { type: "timestamp", notNull: true },
+    updated_at: { type: "timestamp", notNull: true },
+  });
+
+  pgm.createTable("lessons", {
+    id: { type: "serial", primaryKey: true, unique: true, notNull: true },
+    tutor_id: { type: "serial", notNull: true, references: "users(id)" },
+    student_id: { type: "serial", notNull: true, references: "users(id)" },
+    slot_id: { type: "serial", notNull: true, references: "slots(id)" },
+    zoom_meeting_id: { type: "bigint", notNull: true, unique: true },
+    start: { type: "timestamp", notNull: true },
+    end: { type: "timestamp", notNull: true },
+    meeting_url: { type: "varchar(255)", notNull: true },
+    created_at: { type: "timestamp", notNull: true },
+    updated_at: { type: "timestamp", notNull: true },
+  });
+
+  pgm.createIndex("lessons", "id");
+  pgm.createIndex("slots", "id");
   pgm.createIndex("tutors", "id");
   pgm.createIndex("users", "id");
 };
@@ -45,9 +84,18 @@ exports.up = (pgm) => {
  * @returns {Promise<void> | void}
  */
 exports.down = (pgm) => {
+  // indexes
+  pgm.dropIndex("lessons", "id", { ifExists: true });
+  pgm.dropIndex("slots", "id", { ifExists: true });
   pgm.dropIndex("tutors", "id", { ifExists: true });
   pgm.dropIndex("users", "id", { ifExists: true });
+
+  // tables
+  pgm.dropTable("lessons", { ifExists: true });
+  pgm.dropTable("slots", { ifExists: true });
   pgm.dropTable("tutors", { ifExists: true });
   pgm.dropTable("users", { ifExists: true });
+  // types
   pgm.dropType("user_type", { ifExists: true });
+  pgm.dropType("repeat_type", { ifExists: true });
 };
