49b08a79dca3342bddb8bf01d69cf71f8070b64f
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-05-27 16:42:25 +0300

    feat(client): add tailwindcss, redux, and react-router-dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "@/pages/Root";
+import ErrorPage from "@/pages/Error";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "@/redux/store";
+import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  { path: "/", element: <Root />, errorElement: <ErrorPage /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ReduxProvider store={store}>
+      <RouterProvider router={router} />
+    </ReduxProvider>
+  </React.StrictMode>
+);
