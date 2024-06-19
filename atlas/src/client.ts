2f755f69ab5243a03cf1d76aa765e430d8fd348c
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-06-19 10:04:54 +0300

    feat(atlas): inspect error response

--- atlas/src/client.ts
+++ atlas/src/client.ts
@@ -1,24 +1,39 @@
 import { Backend } from "@litespace/types";
-import axios, { AxiosInstance } from "axios";
-
-const hostname = window.location.hostname;
+import axios, { AxiosError, AxiosInstance } from "axios";
 
 export const backendApiUrls: Record<Backend, string> = {
-  [Backend.Local]: `http://${hostname}:8080`,
+  [Backend.Local]: `http://localhost:8080`,
   [Backend.Staging]: "https://moon.litespace.com",
   [Backend.Production]: "https://mars.litespace.com",
 };
 
 export const backendSockets = {
-  [Backend.Local]: `ws://${hostname}:8080`,
+  [Backend.Local]: `ws://localhost:8080`,
   [Backend.Staging]: "wss://moon.litespace.com",
   [Backend.Production]: "wss://mars.litespace.com",
 };
 
 export function createClient(backend: Backend): AxiosInstance {
-  return axios.create({
+  const client = axios.create({
     baseURL: backendApiUrls[backend],
     withCredentials: true,
     headers: { "Content-Type": "application/json" },
   });
+
+  client.interceptors.response.use(undefined, (error: AxiosError) => {
+    const data = error.response?.data;
+    const message =
+      data &&
+      typeof data === "object" &&
+      "message" in data &&
+      typeof data.message === "string"
+        ? data.message
+        : null;
+
+    if (message) return Promise.reject(new Error(message));
+
+    return Promise.reject(error);
+  });
+
+  return client;
 }
