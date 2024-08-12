553d913a78aa7598ba4cedc7b6634256524512a9
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-08-12 22:27:59 +0300

    feat(nova,luna): show error & success notification on booking an interview

--- nova/src/pages/TutorOnboarding.tsx
+++ nova/src/pages/TutorOnboarding.tsx
@@ -4,13 +4,20 @@ import dayjs from "@/lib/dayjs";
 import { flatten } from "lodash";
 import React, { useMemo, useState } from "react";
 import { useMutation, useQuery } from "react-query";
-import cn from "classnames";
 import { splitSlot } from "@litespace/sol";
 import { ICall, ISlot } from "@litespace/types";
-import { Button, ButtonType, DatePicker } from "@litespace/luna";
+import {
+  Button,
+  ButtonType,
+  DatePicker,
+  messages,
+  toaster,
+} from "@litespace/luna";
 import { Dayjs } from "dayjs";
+import { useIntl } from "react-intl";
 
 const TutorOnboarding: React.FC = () => {
+  const intl = useIntl();
   const [date, setDate] = useState<Dayjs>(dayjs());
   const [selectedSlot, setSelectedSlot] = useState<ISlot.Discrete | null>(null);
   const interviewer = useQuery({
@@ -30,6 +37,21 @@ const TutorOnboarding: React.FC = () => {
 
   const mutation = useMutation({
     mutationFn: (payload: ICall.CreateApiPayload) => atlas.call.create(payload),
+    onSuccess() {
+      toaster.success({
+        title: intl.formatMessage({
+          id: messages["page.tutor.onboarding.book.interview.success.title"],
+        }),
+      });
+    },
+    onError(error) {
+      toaster.error({
+        title: intl.formatMessage({
+          id: messages["page.tutor.onboarding.book.interview.fail.title"],
+        }),
+        description: error instanceof Error ? error.message : undefined,
+      });
+    },
   });
 
   const daySlots: ISlot.Discrete[] = useMemo(() => {
@@ -70,6 +92,7 @@ const TutorOnboarding: React.FC = () => {
         <div>
           <DatePicker
             min={dayjs()}
+            max={dayjs().add(14, "days")}
             selected={date}
             onSelect={(date) => setDate(dayjs(date.format("YYYY-MM-DD")))}
           />
@@ -80,26 +103,55 @@ const TutorOnboarding: React.FC = () => {
             {date.format("dddd، DD MMMM، YYYY")}
           </h3>
 
-          <ul className="w-[200px] flex flex-col gap-3">
-            {selectableSlots.map((slot) => {
-              return (
-                <li key={slot.start}>
-                  <Button
-                    onClick={() => setSelectedSlot(slot)}
-                    type={
-                      selectedSlot &&
-                      dayjs(slot.start).isSame(selectedSlot.start, "minutes")
-                        ? ButtonType.Primary
-                        : ButtonType.Secondary
-                    }
-                  >
-                    {dayjs(slot.start).format("hh:mm a")}
-                  </Button>
-                </li>
-              );
-            })}
+          <ul className="w-[200px] flex flex-col gap-3 h-[400px] overflow-y-scroll scrollbar-none relative">
+            {selectableSlots
+              .filter((slot) => dayjs(slot.start).isAfter(dayjs(), "minutes"))
+              .map((slot) => {
+                return (
+                  <li key={slot.start}>
+                    <Button
+                      onClick={() => setSelectedSlot(slot)}
+                      type={
+                        selectedSlot &&
+                        dayjs(slot.start).isSame(selectedSlot.start, "minutes")
+                          ? ButtonType.Primary
+                          : ButtonType.Secondary
+                      }
+                    >
+                      {dayjs(slot.start).format("hh:mm a")}
+                    </Button>
+                  </li>
+                );
+              })}
           </ul>
         </div>
+      </div>
+
+      <div className="w-[250px] mt-12">
+        <Button
+          onClick={() => {
+            if (!selectedSlot) return;
+            mutation.mutate({
+              type: ICall.Type.Interview,
+              slotId: selectedSlot.id,
+              start: selectedSlot.start,
+              duration: 30,
+            });
+          }}
+          disabled={!selectedSlot || mutation.isLoading}
+          loading={mutation.isLoading}
+        >
+          <span className="truncate">
+            {intl.formatMessage(
+              {
+                id: messages[
+                  "page.tutor.onboarding.book.interview.button.label"
+                ],
+              },
+              { name: interviewer.data.name.ar }
+            )}
+          </span>
+        </Button>
       </div>
     </div>
   );
