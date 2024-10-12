1422c5395bbe1ffb70c64da1fd1b7c813b99da4a
Ahmed <me@ahmedibrahim.dev>
2024-10-12 10:53:48 +0300

    feat(mobile): add login screen

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,25 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
+import React from "react";
+import { Pressable, StyleSheet } from "react-native";
+
+const Home: React.FC = () => {
+  return (
+    <ThemedView>
+      <ThemedView style={styles.container}>
+        <Pressable>
+          <Link href="/login">Login</Link>
+        </Pressable>
+      </ThemedView>
+    </ThemedView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+  },
+});
+
+export default Home;
