610d9a1269537bf6974dd66d92389e0b71474b35
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2024-10-14 16:40:35 +0300

    feat(dashboard): implement plan delete and edit actions

--- apps/dashboard/src/components/common/BooleanField.tsx
+++ apps/dashboard/src/components/common/BooleanField.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Check, X } from "react-feather";
 
 const BooleanField: React.FC<{ checked: boolean }> = ({ checked }) => {
-  return checked ? <Check /> : <X />;
+  return (
+    <div className="flex justify-center items-center">
+      {checked ? <Check /> : <X />}
+    </div>
+  );
 };
 
 export default BooleanField;
