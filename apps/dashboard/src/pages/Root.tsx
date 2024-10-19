24466257c66d1f05e2343a908596422f961db0ed
mohamedali <elasasmohamed97@gmail.com>
2024-10-19 03:07:07 +0300

    feat(dashboard): implement create user form

--- apps/dashboard/src/pages/Root.tsx
+++ apps/dashboard/src/pages/Root.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Toaster } from "@litespace/luna";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useAppSelector } from "@/redux/store";
 import { profileSelectors } from "@/redux/user/profile";
 import { IUser } from "@litespace/types";
@@ -9,16 +9,17 @@ import { Route } from "@/lib/route";
 const Root: React.FC = () => {
   const profile = useAppSelector(profileSelectors.user);
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    if (!profile) return;
+    if (!profile || location.pathname !== Route.Root) return;
     if (
       profile.role === IUser.Role.SuperAdmin ||
       profile.role === IUser.Role.RegularAdmin
     )
       navigate(Route.Users);
     if (profile.role === IUser.Role.MediaProvider) navigate(Route.Media);
-  }, [navigate, profile]);
+  }, [location.pathname, navigate, profile]);
 
   return (
     <main className="min-h-screen text-foreground flex overflow-y-hidden">
