94560e735a4da7cb2c3004514b0ca5b19693797f
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-10-20 09:09:21 +0300

    feat(server): define and replace server/client socket event types

--- packages/types/src/wss.ts
+++ packages/types/src/wss.ts
@@ -1,3 +1,5 @@
+import { IMessage, IRule, ITutor } from "@/index";
+
 /**
  * Events emitted by the client
  */
@@ -36,9 +38,57 @@ export enum ServerEvent {
   RuleUpdated = "RuleUpdated",
   RuleCreated = "RuleCreated",
   InvoiceUpdated = "InvoiceUpdated",
+  InvoiceDeleted = "InvoiceDeleted",
 }
 
 export enum Room {
   TutorsCache = "TutorsCache",
   AdminInvoices = "AdminInvoices",
 }
+
+type EventCallback<T> = (arg: T) => Promise<void> | void;
+
+/**
+ * Events emitted by the client
+ */
+export type ClientEventsMap = {
+  [ClientEvent.SendMessage]: EventCallback<{ roomId: number; text: string }>;
+  [ClientEvent.UpdateMessage]: EventCallback<{ id: number; text: string }>;
+  [ClientEvent.DeleteMessage]: EventCallback<{ id: number }>;
+  [ClientEvent.PeerOpened]: EventCallback<{ callId: number; peerId: number }>;
+  [ClientEvent.ToggleCamera]: EventCallback<{ call: number; camera: boolean }>;
+  [ClientEvent.ToggleMic]: EventCallback<{ call: number; mic: boolean }>;
+};
+
+/**
+ * Events emitted by the server
+ */
+export type ServerEventsMap = {
+  [ServerEvent.RoomMessage]: EventCallback<IMessage.Self>;
+  [ServerEvent.RoomMessageUpdated]: EventCallback<IMessage.Self>;
+  [ServerEvent.RoomMessageDeleted]: EventCallback<{
+    roomId: number;
+    messageId: number;
+  }>;
+  [ServerEvent.UserJoinedCall]: EventCallback<{ peerId: string }>;
+  [ServerEvent.CameraToggled]: EventCallback<{ user: number; camera: boolean }>;
+  [ServerEvent.MicToggled]: EventCallback<{ user: number; mic: boolean }>;
+  [ServerEvent.MessageRead]: EventCallback<{ messageId: number }>;
+  [ServerEvent.UserStatusChanged]: EventCallback<{ online: boolean }>;
+  [ServerEvent.InvoiceUpdated]: EventCallback<void>;
+  [ServerEvent.InvoiceDeleted]: EventCallback<void>;
+  [ServerEvent.LessonBooked]: EventCallback<{
+    tutor: number;
+    rule: number;
+    events: IRule.RuleEvent[];
+  }>;
+  [ServerEvent.LessonCanceled]: EventCallback<{
+    tutor: number;
+    rule: number;
+    events: IRule.RuleEvent[];
+  }>;
+  [ServerEvent.RuleCreated]: EventCallback<void>;
+  [ServerEvent.RuleUpdated]: EventCallback<void>;
+  [ServerEvent.RuleDeleted]: EventCallback<void>;
+  [ServerEvent.TutorUpdated]: EventCallback<ITutor.FullTutor>;
+};
