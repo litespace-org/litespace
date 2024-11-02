a0326264bb30fb632c9099a932f4bdeb61a94a7a
mostafa <mostafakamar.dev@gmail.com>
2024-11-02 03:44:05 +0200

    fix(luna): modified build flow to include css files

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
-import  Spinner  from "@litespace/luna/icons/spinner";
-import {  locales } from "@litespace/luna/locales";
-import {   backend, getToken } from "@litespace/luna/lib";
+import Spinner from "@litespace/luna/icons/spinner";
+import { locales } from "@litespace/luna/locales";
+import { backend, getToken } from "@litespace/luna/lib";
 import { IntlProvider } from "react-intl";
 import { store, persistor } from "@/redux/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -11,7 +11,6 @@ import { BackendProvider } from "@litespace/headless/backend";
 import { AtlasProvider } from "@litespace/headless/atlas";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "@/App.tsx";
-
 import "@litespace/luna/style.css";
 import "@litespace/luna/tailwind.css";
 import "react-toastify/dist/ReactToastify.min.css";
