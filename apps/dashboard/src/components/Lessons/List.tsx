c2aa4c82d77a723ef6047dcc4fb71950f9395142
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-11-06 21:07:56 +0200

    feat(dashboard): impl. lessons filters

--- apps/dashboard/src/components/Lessons/List.tsx
+++ apps/dashboard/src/components/Lessons/List.tsx
@@ -4,12 +4,14 @@ import { createColumnHelper } from "@tanstack/react-table";
 import { ILesson, Void } from "@litespace/types";
 import { Table } from "@/components/common/Table";
 import { UseQueryResult } from "@tanstack/react-query";
-import { Check, User } from "react-feather";
-import { Link } from "react-router-dom";
-import { asFullAssetUrl } from "@litespace/luna/backend";
 import { formatCurrency } from "@litespace/luna/utils";
 import { Duration } from "@litespace/sol/duration";
-import { asIsoDate } from "@litespace/sol/dayjs";
+import { Loading } from "@litespace/luna/Loading";
+import Error from "@/components/common/Error";
+import { price } from "@litespace/sol/value";
+import UserPopover from "@/components/common/UserPopover";
+import DateField from "@/components/common/DateField";
+import { dayjs } from "@/lib/dayjs";
 
 const List: React.FC<{
   query: UseQueryResult<ILesson.FindUserLessonsApiResponse, Error>;
@@ -25,110 +27,86 @@ const List: React.FC<{
     createColumnHelper<ILesson.FindUserLessonsApiResponse["list"][number]>();
 
   const columns = useMemo(() => {
-    const currentTime = new Date();
-
     return [
       columnHelper.accessor("lesson.id", {
         header: intl("global.labels.id"),
         cell: (info) => info.getValue(),
       }),
+      columnHelper.accessor((row) => row.members, {
+        header: intl("dashboard.lessons.members"),
+        cell: (info) => {
+          const members = info.getValue();
+          return (
+            <div className="flex flex-row items-start justify-start gap-2">
+              {members.map((member) => (
+                <UserPopover id={member.userId} key={member.userId} />
+              ))}
+            </div>
+          );
+        },
+      }),
       columnHelper.accessor("lesson.price", {
         header: intl("dashboard.lessons.price"),
-        cell: (info) => formatCurrency(info.getValue()),
-      }),
-      columnHelper.accessor((row) => row.members, {
-        header: intl("dashboard.lessons.members.name"),
-        cell: (info) => {
-          const members = info.getValue();
-          return (
-            <div className="grid gap-1">
-              {members.map((member, index) => (
-                <Link
-                  to={`/user/${member.userId}`}
-                  key={index}
-                  className="flex items-center gap-1 p-2 duration-300 rounded-md whitespace-nowrap hover:bg-gray-400/50"
-                >
-                  {member.image ? (
-                    <img
-                      src={asFullAssetUrl(member.image)}
-                      className="w-6 h-6 rounded-full "
-                    />
-                  ) : (
-                    <User className="w-6 h-6" />
-                  )}
-                  {member.name}
-                </Link>
-              ))}
-            </div>
-          );
-        },
-      }),
-      columnHelper.accessor((row) => row.members, {
-        header: intl("dashboard.lessons.members.host"),
-        cell: (info) => {
-          const members = info.getValue();
-          return (
-            <div className="grid">
-              {members.map((member, index) => (
-                <div
-                  key={index}
-                  className="flex justify-center text-2xl text-center"
-                >
-                  {member.host ? (
-                    <Check className="w-8 h-8 text-green-600" />
-                  ) : (
-                    "-"
-                  )}
-                </div>
-              ))}
-            </div>
-          );
-        },
+        cell: (info) => formatCurrency(price.unscale(info.getValue())),
       }),
       columnHelper.accessor("call.duration", {
-        header: intl("dashboard.lessons.call.duration"),
-        cell: (info) => Duration.from(`${info.getValue()}min`).format("ar"),
+        header: intl("dashboard.lessons.duration"),
+        cell: (info) => Duration.from(info.getValue().toString()).format("ar"),
       }),
       columnHelper.accessor("call.start", {
-        header: intl("dashboard.lessons.call.start"),
-        cell: (info) => (
-          <div className="whitespace-nowrap">{asIsoDate(info.getValue())}</div>
-        ),
+        header: intl("dashboard.lessons.start"),
+        cell: (info) => <DateField date={info.getValue()} />,
       }),
-      columnHelper.accessor((row) => row, {
-        header: intl("dashboard.lessons.call.timeline-status"),
+      columnHelper.display({
+        header: intl("dashboard.lessons.status"),
         cell: (info) => {
-          const row = info.getValue();
-          const callStartDate = new Date(row.call.start);
-          const callEndDate = new Date(
-            new Date(row.call.start).getTime() + row.call.duration * 60 * 1000
-          );
-          if (currentTime < callStartDate) {
-            return intl("dashboard.lessons.call.timeline-status.future");
-          } else if (currentTime > callEndDate) {
-            return intl("dashboard.lessons.call.timeline-status.past");
-          } else {
-            return intl("dashboard.lessons.call.timeline-status.now");
-          }
+          const canceledAt = info.row.original.lesson.canceledAt;
+          const canceledBy = info.row.original.lesson.canceledBy;
+
+          if (canceledAt && canceledBy)
+            return (
+              <div className="flex flex-col items-start gap-1">
+                <p>{intl("dashboard.lessons.status.cancelled")}</p>
+                <UserPopover id={canceledBy} />
+              </div>
+            );
+
+          const start = dayjs(info.row.original.call.start);
+          const end = start.add(info.row.original.call.duration, "minutes");
+          const now = dayjs();
+
+          const happening = now.isBetween(start, end, "minutes", "[]");
+          if (happening) return intl("dashboard.lessons.status.happening");
+
+          const pending = start.isAfter(dayjs());
+          if (pending) return intl("dashboard.lessons.status.pending");
+
+          const fullfilled = start.isBefore(dayjs());
+          if (fullfilled) return intl("dashboard.lessons.status.fulfilled");
+
+          return "-";
         },
       }),
-      columnHelper.accessor((row) => row, {
-        header: intl("dashboard.lessons.call.status"),
-        cell: (info) => {
-          const row = info.getValue();
-          const cancellationDate = row.call.canceledAt;
-          const callDate = new Date(row.call.start);
-          if (cancellationDate)
-            return intl("dashboard.lessons.call.status.cancelled");
-          if (callDate < currentTime) {
-            return intl("dashboard.lessons.call.status.fullfilled");
-          } else {
-            return intl("dashboard.lessons.call.status.pending");
-          }
-        },
+      columnHelper.accessor("lesson.createdAt", {
+        header: intl("global.created-at"),
+        cell: (info) => <DateField date={info.getValue()} />,
+      }),
+      columnHelper.accessor("lesson.updatedAt", {
+        header: intl("global.updated-at"),
+        cell: (info) => <DateField date={info.getValue()} />,
       }),
     ];
   }, [columnHelper, intl]);
+
+  if (query.isLoading) return <Loading className="h-1/4" />;
+  if (query.error)
+    return (
+      <Error
+        title={intl("dashboard.error.alert.title")}
+        error={query.error}
+        refetch={query.refetch}
+      />
+    );
 
   if (!query.data) return null;
 
