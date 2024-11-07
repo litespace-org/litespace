8c0eb0c8f5e4e0476d4c44c50b55ecb131a38c30
mostafakamar2308 <moustafa.31011197@med.tanta.edu.eg>
2024-11-07 22:07:09 +0200

    refactor(nova): integrate app config

--- apps/nova/src/pages/Lessons.tsx
+++ apps/nova/src/pages/Lessons.tsx
@@ -3,51 +3,52 @@ import { profileSelectors } from "@/redux/user/profile";
 import React from "react";
 import List from "@/components/Lessons/List";
 import { Button, ButtonSize } from "@litespace/luna/Button";
-import { Spinner } from "@litespace/luna/Spinner";
 import { useFormatMessage } from "@litespace/luna/hooks/intl";
 import { isEmpty } from "lodash";
 import Empty from "@/components/Lessons/Empty";
 import Error from "@/components/Lessons/Error";
 import { useFindUserLessons } from "@litespace/headless/lessons";
+import { Loading } from "@litespace/luna/Loading";
 
 const Lessons: React.FC = () => {
   const intl = useFormatMessage();
   const profile = useAppSelector(profileSelectors.user);
 
-  const { query: lessons, list, more } = useFindUserLessons(profile?.id);
+  const { query, ...pagination } = useFindUserLessons(profile?.id);
 
   return (
     <div className="w-full px-8 py-10 mx-auto max-w-screen-2xl">
-      {profile && list && isEmpty(list) ? (
+      {query.isLoading ? <Loading /> : null}
+      {profile && query.data && isEmpty(query.data.list) ? (
         <div className="mb-4">
           <Empty role={profile.role} />
         </div>
       ) : null}
 
-      {lessons.isLoading ? (
-        <div className="flex items-center justify-center h-1/2">
-          <Spinner />
-        </div>
-      ) : null}
+      {query.error ? <Error error={query.error} /> : null}
 
-      {lessons.error ? <Error error={lessons.error} /> : null}
-
-      {lessons && list && profile ? (
+      {query.data?.list && profile ? (
         <div>
-          <List list={list} user={profile} />
-          {!isEmpty(list) ? (
-            <div className="mr-6">
+          <List list={query.data?.list} user={profile} />
+          {!isEmpty(query.data.list) ? (
+            <div className="mr-6 flex items-center gap-4">
               <Button
                 size={ButtonSize.Small}
-                loading={lessons.isLoading || lessons.isFetching}
-                disabled={
-                  lessons.isLoading ||
-                  lessons.isFetching ||
-                  !lessons.hasNextPage
-                }
-                onClick={more}
+                loading={query.isFetching}
+                disabled={query.isFetching || pagination.page === 1}
+                onClick={pagination.prev}
               >
-                {intl("global.labels.more")}
+                {intl("global.labels.previous")}
+              </Button>
+              <Button
+                size={ButtonSize.Small}
+                loading={query.isFetching}
+                disabled={
+                  query.isFetching || pagination.page === pagination.totalPages
+                }
+                onClick={pagination.next}
+              >
+                {intl("global.labels.next")}
               </Button>
             </div>
           ) : null}
