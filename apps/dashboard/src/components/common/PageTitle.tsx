7381b45e8e69861aac661e15de4d326ff9c2865c
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-11-09 07:16:12 +0200

    feat(dash): impl. labels table

--- apps/dashboard/src/components/common/PageTitle.tsx
+++ apps/dashboard/src/components/common/PageTitle.tsx
@@ -2,26 +2,21 @@ import React from "react";
 import RecordsCount from "@/components/common/RecordsCount";
 import { Spinner } from "@litespace/luna/Spinner";
 import cn from "classnames";
+import { Typography } from "@litespace/luna/Typography";
 
 const PageTitle: React.FC<{
   title: string;
   fetching: boolean;
   count?: number;
-  variant?: "primary" | "secondary";
   className?: string;
-}> = ({ title, fetching, count, className, variant = "primary" }) => {
+}> = ({ title, fetching, count, className }) => {
   return (
     <div className={cn("flex flex-row items-center gap-4", className)}>
-      <h1
-        className={`${variant === "primary" ? "text-2xl" : ""} ${
-          variant === "secondary" ? "text-xl" : ""
-        }`}
-      >
+      <Typography element="h4">
         {title}
         &nbsp;
         {count ? <RecordsCount count={count} /> : null}
-      </h1>
-
+      </Typography>
       {fetching ? <Spinner /> : null}
     </div>
   );
