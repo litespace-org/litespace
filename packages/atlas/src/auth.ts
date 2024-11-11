ba4ab7954975e783966a6453e20130f12afc0d5a
mostafakamar2308 <moustafa.31011197@med.tanta.edu.eg>
2024-11-11 16:20:20 +0200

    fix(atlas): fixed verify email endpoint

--- packages/atlas/src/auth.ts
+++ packages/atlas/src/auth.ts
@@ -32,6 +32,6 @@ export class Auth extends Base {
   }
 
   async verifyEmail(token: string): Promise<void> {
-    await this.post("/api/v1/auth/verify-email", { token });
+    await this.put("/api/v1/auth/verify-email", { token });
   }
 }
