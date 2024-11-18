1bf716dd44dd782f81b96b85edcecc7f853abdc7
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-11-18 13:05:53 +0200

    feat(nova): render call page streams & enable full screen

--- apps/nova/src/pages/Call.tsx
+++ apps/nova/src/pages/Call.tsx
@@ -5,44 +5,41 @@ import {
   ButtonType,
   ButtonVariant,
 } from "@litespace/luna/Button";
-import { Drawer } from "@litespace/luna/Drawer";
-import { useMediaQueries } from "@litespace/luna/hooks/media";
-import { useRender } from "@litespace/luna/hooks/common";
-import { useFormatMessage } from "@litespace/luna/hooks/intl";
-import {
-  // Mic,
-  // MicOff,
-  // Video,
-  // VideoOff,
-  // Monitor,
-  MessageCircle,
-  Phone,
-  // Maximize,
-  // Minimize,
-} from "react-feather";
+// import { Drawer } from "@litespace/luna/Drawer";
+// import { useMediaQueries } from "@litespace/luna/hooks/media";
+// import { useRender } from "@litespace/luna/hooks/common";
+// import { useFormatMessage } from "@litespace/luna/hooks/intl";
 import { useParams } from "react-router-dom";
 import cn from "classnames";
 // import { useFullScreen } from "@/hooks/call";
 // import Media from "@/components/Call/Media";
-import Messages from "@/components/Chat/Messages";
+// import Messages from "@/components/Chat/Messages";
 import { useAppSelector } from "@/redux/store";
 import { profileSelectors } from "@/redux/user/profile";
-import { orNull } from "@litespace/sol/utils";
+import { orNull, orUndefined } from "@litespace/sol/utils";
 import {
   // useCall,
   useCallV2,
   useFindCallRoomById,
+  useFullScreen,
   usePeerIds,
 } from "@litespace/headless/calls";
 import { useDisplayRecorder } from "@litespace/headless/recorder";
 import { isGhost } from "@/lib/ghost";
+import CallView, { CallViewProps } from "@/components/Call/CallView";
+import CallIncoming from "@litespace/assets/CallIncoming";
+import Monitor from "@litespace/assets/Monitor";
+import Video from "@litespace/assets/Video";
+import Microphone from "@litespace/assets/Microphone";
+import VideoSlash from "@litespace/assets/VideoSlash";
+import MicrophoneSlash from "@litespace/assets/MicrophoneSlash";
 // import GhostView from "@/components/Call/GhostView";
 
 const Call: React.FC = () => {
   const profile = useAppSelector(profileSelectors.user);
-  const chat = useRender();
-  const intl = useFormatMessage();
-  const mediaQueries = useMediaQueries();
+  // const chat = useRender();
+  // const intl = useFormatMessage();
+  // const mediaQueries = useMediaQueries();
   const { id } = useParams<{ id: string }>();
   // const { isFullScreen, toggleFullScreen, ref } = useFullScreen();
 
@@ -59,13 +56,13 @@ const Call: React.FC = () => {
     return callRoom.data.members.find((member) => member.id !== profile?.id);
   }, [callRoom.data, profile?.id]);
 
-  const messages = useMemo(
-    () =>
-      callRoom.data ? (
-        <Messages room={callRoom.data.room} members={callRoom.data.members} />
-      ) : null,
-    [callRoom.data]
-  );
+  // const messages = useMemo(
+  //   () =>
+  //     callRoom.data ? (
+  //       <Messages room={callRoom.data.room} members={callRoom.data.members} />
+  //     ) : null,
+  //   [callRoom.data]
+  // );
 
   // const { user, mate, start, onToggleCamera, onToggleMic, peer, ghostStreams } =
   //   useCall(
@@ -116,6 +113,8 @@ const Call: React.FC = () => {
   // const ghostPeerId = useFindPeerId(findGhostPeerIdQuery);
   // const tutorPeerId = useFindPeerId(findTutorPeerIdQuery);
 
+  const fullScreen = useFullScreen<HTMLDivElement>();
+
   const peers = usePeerIds(
     useMemo(
       () => ({
@@ -123,12 +122,13 @@ const Call: React.FC = () => {
         isGhost,
         mateUserId: orNull(mateInfo?.id),
         role: orNull(profile?.role),
+        disableGhost: true,
       }),
       [callId, mateInfo?.id, profile?.role]
     )
   );
 
-  useCallV2(
+  const { userMedia, mateStream, mateScreenStream } = useCallV2(
     useMemo(
       () => ({
         isGhost,
@@ -140,29 +140,64 @@ const Call: React.FC = () => {
     )
   );
 
-  console.log({ ghost: peers.ghost.data, tutor: peers.tutor.data });
+  const callViewProps: CallViewProps = useMemo(
+    () => ({
+      user: {
+        streams: {
+          self: userMedia.stream,
+          screen: null,
+        },
+        speaking: true,
+        name: orUndefined(profile?.name),
+      },
+      mate: {
+        streams: {
+          self: mateStream,
+          screen: mateScreenStream,
+        },
+        speaking: true,
+        name: orUndefined(mateInfo?.name),
+      },
+      fullScreen: {
+        enabled: fullScreen.enabled,
+        start: fullScreen.start,
+        exit: fullScreen.exit,
+      },
+    }),
+    [
+      fullScreen.enabled,
+      fullScreen.exit,
+      fullScreen.start,
+      mateInfo?.name,
+      mateScreenStream,
+      mateStream,
+      profile?.name,
+      userMedia.stream,
+    ]
+  );
 
   return (
     <div
+      ref={fullScreen.ref}
       className={cn(
-        "flex overflow-hidden w-full",
-        isGhost ? "h-screen" : "h-[calc(100vh-4rem)]"
+        "flex overflow-hidden w-full h-screen max-w-screen-lg mx-auto bg-natural-50"
       )}
     >
       <div
         id="call-page"
-        // ref={ref}
         className={cn(
           "flex flex-col w-full h-full",
-          "transition-all duration-300 bg-dash-sidebar"
+          "transition-all duration-300"
         )}
       >
         <div
           className={cn(
-            "relative flex-1 w-full",
-            isGhost ? "p-0" : "max-h-[calc(100%-110px)] pt-10 px-4"
+            "relative flex-1 w-full mb-10",
+            fullScreen.enabled ? "mb-0" : "mb-10"
+            // isGhost ? "p-0" : "max-h-[calc(100%-110px)] pt-10 px-4"
           )}
         >
+          <CallView {...callViewProps} />
           {/* {isGhost ? (
             <GhostView streams={ghostStreams} />
           ) : (
@@ -186,26 +221,62 @@ const Call: React.FC = () => {
             />
           )} */}
         </div>
+
+        {!fullScreen.enabled ? (
+          <div className="h-[1px] w-full bg-natural-400" />
+        ) : null}
+
         {!isGhost ? (
-          <div className="flex items-center justify-center gap-4 my-10">
+          <div
+            className={cn(
+              "flex items-center justify-center gap-6 mb-12",
+              fullScreen.enabled ? "mt-6" : "mt-10"
+            )}
+          >
             <Button
               onClick={onLeaveCall}
               size={ButtonSize.Small}
               type={ButtonType.Error}
+              variant={ButtonVariant.Secondary}
             >
-              <Phone className="w-[20px] h-[20px]" />
+              <CallIncoming className="[&_*]:stroke-destructive-700" />
             </Button>
 
-            {!mediaQueries.lg ? (
-              <Button
-                onClick={chat.show}
-                size={ButtonSize.Small}
-                type={ButtonType.Main}
-                variant={ButtonVariant.Secondary}
-              >
-                <MessageCircle />
-              </Button>
-            ) : null}
+            <Button
+              onClick={() => alert("todo")}
+              size={ButtonSize.Small}
+              type={ButtonType.Success}
+              variant={ButtonVariant.Secondary}
+            >
+              <Monitor className="[&_*]:stroke-brand-700" />
+            </Button>
+
+            <Button
+              onClick={userMedia.toggleCamera}
+              size={ButtonSize.Small}
+              type={ButtonType.Main}
+              variant={ButtonVariant.Secondary}
+            >
+              {userMedia.video ? (
+                <Video className="[&_*]:stroke-brand-700" />
+              ) : (
+                <VideoSlash className="[&_*]:stroke-brand-700" />
+              )}
+            </Button>
+
+            <Button
+              onClick={userMedia.toggleMic}
+              size={ButtonSize.Small}
+              type={ButtonType.Main}
+              variant={ButtonVariant.Secondary}
+            >
+              {userMedia.audio ? (
+                <Microphone className="[&_*]:stroke-brand-700" />
+              ) : (
+                <MicrophoneSlash className="[&_*]:stroke-brand-700" />
+              )}
+            </Button>
+
             {/* <Button
             type={ButtonType.Main}
             variant={ButtonVariant.Secondary}
@@ -262,7 +333,7 @@ const Call: React.FC = () => {
         ) : null}
       </div>
 
-      {!isGhost ? (
+      {/* {!isGhost ? (
         <div
           className={cn(
             "hidden w-full lg:flex lg:flex-col lg:max-w-[350px] xl:max-w-[450px]"
@@ -278,7 +349,7 @@ const Call: React.FC = () => {
         close={chat.hide}
       >
         {messages}
-      </Drawer>
+      </Drawer> */}
     </div>
   );
 };
