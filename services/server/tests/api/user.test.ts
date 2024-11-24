d40c9573408d25dfea1d3ba0b7dd4d974ddf400e
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-11-24 14:05:42 +0200

    test(server): refactor user tests

--- services/server/tests/api/user.test.ts
+++ services/server/tests/api/user.test.ts
@@ -1,7 +1,9 @@
 import { flush } from "@fixtures/shared";
 import { IUser } from "@litespace/types";
-import api, { createUser, createAdmin } from "@fixtures/api";
+import { Api } from "@fixtures/api";
+import db from "@fixtures/db";
 import { expect } from "chai";
+import { safe } from "@litespace/sol/error";
 
 describe("/api/v1/user/", () => {
   beforeEach(async () => {
@@ -12,7 +14,8 @@ describe("/api/v1/user/", () => {
     it("should create a student", async () => {
       const email = "student@example.com";
       const password = "StudentExample101%^&";
-      const result = await api.atlas().user.create({
+      const api = new Api();
+      const result = await api.createUser({
         role: IUser.Role.Student,
         email,
         password,
@@ -25,200 +28,42 @@ describe("/api/v1/user/", () => {
       expect(result.user.role).to.be.eq(IUser.Role.Student);
     });
   });
-});
 
-describe("/api/v1/user/:id", () => {
-  beforeEach(async () => {
-    await flush();
-  });
-  afterAll(async () => {
-    await flush();
-  });
-
-  describe("GET /api/v1/user/:id", () => {
-    it("should get a student", async () => {
-      const admin = await createAdmin();
-
-      const result = await createUser({});
-
-      const user = await api.atlas(admin.token).user.findById(result.user.id);
-      expect(user.id).to.be.eq(result.user.id);
-      expect(user.email).to.be.eq(result.user.email);
-      expect(user.role).to.be.eq(result.user.role);
-    });
-
-    it("shouldn't get a student", async () => {
-      const admin = await createAdmin();
-      try {
-        await api.atlas(admin.token).user.findById(400);
-      } catch (error) {
-        if (error instanceof Error) {
-          expect(error.message).to.be.eq("User not found");
-        }
-      }
-    });
-  });
-
-  describe("PUT /api/v1/user/:id", () => {
-    it("should update a student", async () => {
-      const admin = await createAdmin();
-
-      const created = await createUser({
-        email: "student@example.com",
-        password: "StudentExample101%^&",
-        role: IUser.Role.Student,
+  describe("/api/v1/user/:id", () => {
+    describe("GET /api/v1/user/:id", () => {
+      it("should be able to find a user by id", async () => {
+        const adminApi = await Api.forSuperAdmin();
+        const dbUser = await db.user();
+        const user = await adminApi.atlas.user.findById(dbUser.id);
+        expect(user.id).to.be.eq(dbUser.id);
+        expect(user.email).to.be.eq(dbUser.email);
+        expect(user.role).to.be.eq(dbUser.role);
       });
 
-      const updated = await api
-        .atlas(admin.token)
-        .user.update(created.user.id, {
-          email: "student1@example.com",
-          name: "student updated",
-          gender: IUser.Gender.Male,
-        });
-      expect(updated.email).to.be.eq("student1@example.com");
-      expect(updated.name).to.be.eq("student updated");
-      expect(updated.gender).to.be.eq(IUser.Gender.Male);
-
-      const updated1 = await api
-        .atlas(admin.token)
-        .user.update(created.user.id, {
-          name: "student updated twice",
-          gender: IUser.Gender.Female,
-        });
-      expect(updated1.name).to.be.eq("student updated twice");
-      expect(updated1.gender).to.be.eq(IUser.Gender.Female);
-    });
-  });
-});
-
-describe("/api/v1/user/interviewer", () => {
-  beforeEach(async () => {
-    await flush();
-  });
-  afterAll(async () => {
-    await flush();
-  });
-  describe("GET /interviewer/select", () => {
-    it("should select interviewer", async () => {
-      const tutor = await createUser({ role: IUser.Role.Tutor });
-      const interviewer = await createUser({ role: IUser.Role.Interviewer });
-      const selectedInterviewer = await api
-        .atlas(tutor.token)
-        .user.selectInterviewer();
-      expect(selectedInterviewer.id).to.be.eq(interviewer.user.id);
-    });
-  });
-});
-describe("/api/v1/user/current", () => {
-  describe("GET /current", () => {
-    it("should get current user", async () => {
-      const user = await createUser({ role: IUser.Role.Student });
-      const currentUser = await api.atlas(user.token).user.findCurrentUser();
-      expect(currentUser.user.id).to.be.eq(user.user.id);
-      expect(currentUser.user.role).to.be.eq(user.user.role);
-    });
-  });
-});
-
-describe("/api/v1/user/list", () => {
-  beforeEach(async () => {
-    await flush();
-  });
-  afterAll(async () => {
-    await flush();
-  });
-
-  // ! This Breaks
-  describe("GET /list", () => {
-    it("should get All users", async () => {
-      const admin = await createAdmin();
-      const user1 = await createUser({ role: IUser.Role.Student });
-      const user2 = await createUser({ role: IUser.Role.Student });
-      const user3 = await createUser({ role: IUser.Role.Student });
-      try {
-        const result = await api.atlas(admin.token).user.find({});
-        expect(result.list).to.be.an("array");
-        expect(result.list.length).to.eq(4);
-        expect(result.list.sort((a, b) => a.id - b.id)).to.be.deep.eq([
-          admin.user.id,
-          user1.user.id,
-          user2.user.id,
-          user3.user.id,
-        ]);
-      } catch (error) {
-        console.log(error);
-      }
-    });
-
-    it("should get all students", async () => {
-      const admin = await createAdmin();
-      const user1 = await createUser({ role: IUser.Role.Student });
-      const user2 = await createUser({ role: IUser.Role.Student });
-      const user3 = await createUser({ role: IUser.Role.Student });
-
-      const result = await api
-        .atlas(admin.token)
-        .user.find({ role: IUser.Role.Student });
-      expect(result.list).to.be.an("array");
-      expect(result.list.length).to.eq(3);
-      expect(result.list.sort((a, b) => a.id - b.id)).to.be.deep.eq([
-        user1.user.id,
-        user2.user.id,
-        user3.user.id,
-      ]);
-    });
-  });
-});
-
-describe("/api/v1/user/tutor", () => {
-  beforeEach(async () => {
-    await flush();
-  });
-  afterAll(async () => {
-    await flush();
-  });
-
-  describe("/tutor/meta/:tutorId", () => {
-    it("should get tutor meta", async () => {
-      const tutor = await createUser({ role: IUser.Role.Tutor });
-      const meta = await api
-        .atlas(tutor.token)
-        .user.findTutorMeta(tutor.user.id);
-      expect(meta.id).to.be.eq(tutor.user.id);
-    });
-  });
-
-  // TODO: This requires onboarding function to be called first
-  describe("/tutor/list/onboarded", () => {});
-
-  // TODO: Must Activate Tutor
-  describe("/tutor/stats/:tutor", () => {});
-
-  // TODO: Must Activate Tutor
-  describe("/tutor/activity/:tutor", () => {});
-});
-describe("/api/v1/user/media-provider", () => {
-  describe("/media-provider/tutors", () => {
-    it("should find all tutors for media provider", async () => {
-      const mediaProvider = await createUser({
-        role: IUser.Role.MediaProvider,
+      it("should response with 404 incase user is not found", async () => {
+        const adminApi = await Api.forSuperAdmin();
+        const result = await safe(async () =>
+          adminApi.atlas.user.findById(100)
+        );
+        expect(result).to.be.deep.eq(new Error("User not found"));
       });
+    });
 
-      const t1 = await createUser({ role: IUser.Role.Tutor });
-      const t2 = await createUser({ role: IUser.Role.Tutor });
-      const t3 = await createUser({ role: IUser.Role.Tutor });
-
-      const result = await api
-        .atlas(mediaProvider.token)
-        .user.findTutorsForMediaProvider();
-      expect(result.list).to.be.an("array");
-      expect(result.list.length).to.eq(3);
-      expect(result.list.sort((a, b) => a.id - b.id)).to.be.deep.eq([
-        t1.user.id,
-        t2.user.id,
-        t3.user.id,
-      ]);
+    describe("PUT /api/v1/user/:id", () => {
+      it("should update a user", async () => {
+        const userApi = await Api.forStudent();
+        const u0 = await userApi.atlas.user.findCurrentUser();
+        await userApi.atlas.user.update(u0.user.id, {
+          name: "updated-1",
+        });
+        const u1 = await userApi.atlas.user.findCurrentUser();
+        await userApi.atlas.user.update(u0.user.id, {
+          name: "updated-2",
+        });
+        const u2 = await userApi.atlas.user.findCurrentUser();
+        expect(u1.user.name).to.be.eq("updated-1");
+        expect(u2.user.name).to.be.eq("updated-2");
+      });
     });
   });
 });
