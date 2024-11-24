00676cbdba89b39dc605016df90520a8750aff55
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-11-24 14:05:42 +0200

    test(server): refactor user tests

--- services/server/fixtures/api.ts
+++ services/server/fixtures/api.ts
@@ -3,7 +3,8 @@ import { users } from "@litespace/models";
 import { Backend, IUser } from "@litespace/types";
 import { hashPassword } from "@/lib/user";
 import { faker } from "@faker-js/faker/locale/ar";
-import { sample } from "lodash";
+import { pad, sample } from "lodash";
+import db from "@fixtures/db";
 
 export function atlas(token?: string): Atlas {
   return new Atlas(Backend.Local, () => {
@@ -15,53 +16,42 @@ export function atlas(token?: string): Atlas {
   });
 }
 
+export class Api {
+  public readonly atlas: Atlas;
+  constructor(token?: string) {
+    this.atlas = atlas(token);
+  }
+
+  static async fromCredentials(email: string, password: string) {
+    const result = await atlas().auth.password({ email, password });
+    return new Api(result.token);
+  }
+
+  static async forUser(role: IUser.Role) {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    await db.user({ role: IUser.Role.SuperAdmin, email, password });
+    return await Api.fromCredentials(email, password);
+  }
+
+  static async forSuperAdmin() {
+    return await this.forUser(IUser.Role.SuperAdmin);
+  }
+
+  static async forStudent() {
+    return await this.forUser(IUser.Role.Student);
+  }
+
+  async createUser(payload?: Partial<IUser.CreateApiPayload>) {
+    return this.atlas.user.create({
+      callbackUrl: payload?.callbackUrl || faker.internet.url(),
+      email: payload?.email || faker.internet.email(),
+      password: payload?.password || faker.internet.password(),
+      role: payload?.role || sample(Object.values(IUser.Role))!,
+    });
+  }
+}
+
 export default {
   atlas,
 };
-
-export async function createAdmin() {
-  const email = "admin@litespace.org";
-  const password = "password";
-  await users.create({
-    name: "Admin",
-    email: email,
-    password: hashPassword(password),
-    role: IUser.Role.SuperAdmin,
-    birthYear: 2001,
-  });
-
-  const response = await atlas().auth.password({
-    email,
-    password,
-  });
-
-  return response;
-}
-
-export function gender(): IUser.Gender {
-  return sample([IUser.Gender.Male, IUser.Gender.Female])!;
-}
-
-export async function createUser(
-  payload: Omit<Partial<IUser.Self>, "password"> & {
-    password?: string;
-  }
-) {
-  const password = payload.password || faker.internet.password();
-
-  const user = await users.create({
-    email: payload.email || faker.internet.email(),
-    gender: payload.gender || gender(),
-    name: payload.name || faker.internet.username(),
-    password: hashPassword(password),
-    birthYear: payload.birthYear || faker.number.int({ min: 2000, max: 2024 }),
-    role: payload.role || IUser.Role.Tutor,
-  });
-
-  const response = await atlas().auth.password({
-    email: user.email,
-    password,
-  });
-
-  return response;
-}
