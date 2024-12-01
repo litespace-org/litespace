b09c3ed360343f10ced017b7d60ea7a2493b5806
mostafakamar2308 <moustafa.31011197@med.tanta.edu.eg>
2024-12-01 13:33:56 +0200

    feat(nova): added messages to chat

--- apps/nova/src/pages/Chat.tsx
+++ apps/nova/src/pages/Chat.tsx
@@ -1,17 +1,28 @@
-// import Messages from "@/components/Chat/Messages";
+import Messages from "@/components/Chat/Messages";
 import RoomsContainer from "@/components/Chat/RoomsContainer";
 import React from "react";
 import cn from "classnames";
 import { useSelectedRoom } from "@litespace/luna/hooks/chat";
+import { useFindRoomMembers } from "@litespace/headless/chat";
+import { profileSelectors } from "@/redux/user/profile";
+import { useAppSelector } from "@/redux/store";
+import { asOtherMember } from "@/lib/room";
 
 const Chat: React.FC = () => {
+  const profile = useAppSelector(profileSelectors.user);
+
   const { select, selected } = useSelectedRoom();
+  // TODO: Read/Unread Function
+  const roomMembers = useFindRoomMembers(selected.room);
+  const otherMember = asOtherMember(profile?.id, roomMembers.data);
 
   return (
     <div className={cn("flex flex-row min-h-screen overflow-hidden")}>
       <RoomsContainer selected={selected} select={select} />
-      {/* <Messages room={selected.room} members={selected.members} /> */}
-      <div></div>
+
+      {otherMember && (
+        <Messages room={selected.room} otherMember={otherMember} />
+      )}
     </div>
   );
 };
