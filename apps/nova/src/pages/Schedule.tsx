599ddd051025136b2a411d1708b8c6229a298e14
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-12-03 09:54:44 +0200

    feat(ui): render lessons on the calendar

--- apps/nova/src/pages/Schedule.tsx
+++ apps/nova/src/pages/Schedule.tsx
@@ -1,39 +1,133 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import { Calendar } from "@litespace/luna/Calendar/v2";
 import dayjs from "@/lib/dayjs";
-import { useFindLessons } from "@litespace/headless/lessons";
+import { useInfiniteLessons } from "@litespace/headless/lessons";
 import { useAppSelector } from "@/redux/store";
 import { profileSelectors } from "@/redux/user/profile";
+import Header, { View } from "@/components/Schedule/Header";
+import { AnimatePresence, motion } from "framer-motion";
+import LessonsList from "@/components/UpcomingLessons/Content";
+import { Lessons } from "@litespace/luna/Calendar/v2/Events";
+
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.5 } },
+};
 
 const Schedule: React.FC = () => {
   const [date, setDate] = useState(dayjs().startOf("week"));
+  const [view, setView] = useState<View>("calendar");
   const user = useAppSelector(profileSelectors.user);
 
-  const lessons = useFindLessons(
-    useMemo(
-      () => ({
-        users: user ? [user.id] : [],
-        userOnly: true,
-        size: 1,
-        // todo: filter lessons by date
-      }),
-      [user]
-    )
-  );
-
-  console.log(lessons.query.data);
+  const lessons = useInfiniteLessons({
+    users: user ? [user.id] : [],
+    userOnly: true,
+    after: date.toISOString(),
+    before: date.add(1, "week").toISOString(),
+  });
+  console.log({
+    after: date.toISOString(),
+    before: date.add(1, "week").toISOString(),
+  });
 
   return (
     <div className="w-full p-6 mx-auto overflow-hidden max-w-screen-3xl">
-      <Calendar
-        date={date}
-        nextWeek={() => {
-          setDate((prev) => prev.add(1, "week"));
-        }}
-        prevWeek={() => {
-          setDate((prev) => prev.subtract(1, "week"));
-        }}
-      />
+      <div className="mb-8">
+        <Header
+          date={date}
+          nextWeek={() => {
+            setDate((prev) => prev.add(1, "week"));
+          }}
+          prevWeek={() => {
+            setDate((prev) => prev.subtract(1, "week"));
+          }}
+          view={view}
+          setView={setView}
+        />
+      </div>
+      <AnimatePresence mode="wait" initial={false}>
+        {view === "calendar" ? (
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+          >
+            <Calendar
+              key="calendar"
+              HourView={({ date }) => {
+                if (!lessons.list || !user) return null;
+                const list = lessons.list.filter(({ lesson }) =>
+                  dayjs(lesson.start).isBetween(
+                    date,
+                    date.add(1, "hour"),
+                    "milliseconds",
+                    "[]"
+                  )
+                );
+
+                if (!list.length) return null;
+
+                return (
+                  <div className="p-1">
+                    <Lessons
+                      lessons={list.map(({ lesson, members }) => {
+                        const otherMember = members.find(
+                          (member) => member.userId !== user.id
+                        );
+
+                        if (!otherMember)
+                          throw new Error(
+                            "Other member not found; should never happen"
+                          );
+
+                        return {
+                          id: lesson.id,
+                          otherMember: {
+                            id: otherMember.userId,
+                            image: otherMember.image,
+                            name: otherMember.name,
+                          },
+                          canceled: !!lesson.canceledBy,
+                          start: lesson.start,
+                          end: dayjs(lesson.start)
+                            .add(lesson.duration, "minutes")
+                            .toISOString(),
+                        };
+                      })}
+                      onCancel={() => alert("todo")}
+                      onEdit={() => alert("todo")}
+                      onRebook={() => alert("todo")}
+                    />
+                  </div>
+                );
+              }}
+              date={date}
+            />
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
+
+      <AnimatePresence>
+        {view === "list" ? (
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+          >
+            <LessonsList
+              list={lessons.list}
+              loading={lessons.query.isLoading}
+              error={lessons.query.isError}
+              fetching={lessons.query.isFetching && !lessons.query.isLoading}
+              more={lessons.more}
+              hasMore={lessons.query.hasNextPage && !lessons.query.isPending}
+              key="list"
+            />
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
     </div>
   );
 };
