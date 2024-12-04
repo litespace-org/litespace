58d0f2e4a1f06e5e17c1060f864b657c013224c0
M. E. Abdelsalam <mo.ehab150@gmail.com>
2024-12-04 11:38:31 +0200

    added: endpoint handler for retrieving call members list along with its unit tests.

--- services/server/src/handlers/call.ts
+++ services/server/src/handlers/call.ts
@@ -1,31 +1,68 @@
-import { calls } from "@litespace/models";
 import { NextFunction, Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import { withNamedId } from "@/validation/utils";
-import { groupBy } from "lodash";
-import { forbidden, notfound } from "@/lib/error";
+import { forbidden } from "@/lib/error";
 import { isAdmin, isGhost, isUser } from "@litespace/auth";
+import { CallsController } from "@/controllers/calls";
 
+/*
+  * express handler for retrieving a call object by its id
+  */
 async function findCallById(req: Request, res: Response, next: NextFunction) {
-  const user = req.user;
-  const allowed = isUser(user) || isGhost(user);
-  if (!allowed) return next(forbidden());
+  const controller = new CallsController();
+  try {
+    // parse request params/body/queries
+    const { callId } = withNamedId("callId").parse(req.params);
 
-  const { callId } = withNamedId("callId").parse(req.params);
-  const [call, members] = await Promise.all([
-    calls.findById(callId),
-    calls.findCallMembers([callId]),
-  ]);
-  if (!call) return next(notfound.call());
+    // authorize the request
+    const user = req.user;
+    const allowed = isUser(user) || isGhost(user);
+    if (!allowed) throw forbidden();
 
-  const member =
-    isUser(user) && members.map((member) => member.userId).includes(user.id);
-  const eligible = member || isAdmin(req.user) || isGhost(user);
-  if (!eligible) next(forbidden());
+    const isMember = isUser(user) && await controller.isMember(user.id, callId);
+    const eligible = isMember || isAdmin(req.user) || isGhost(user);
+    if (!eligible) throw forbidden();
 
-  res.status(200).json({ call, members });
+    // retrieve data from the database
+    const call = await controller.getCallById(callId)
+    const members = await controller.getCallMembers(callId);
+
+    // send response to the client
+    res.status(200).json({ call, members });
+  } catch(e) {
+    next(e);
+  }
+}
+
+/*
+  * express handler for retrieving a list of members of a specific call
+  */
+async function findCallMembers(req: Request, res: Response, next: NextFunction) {
+  const controller = new CallsController();
+  try {
+    // parse request params/body/queries
+    const { callId } = withNamedId("callId").parse(req.params);
+
+    // authorize the request
+    const user = req.user;
+    const allowed = isUser(user) || isGhost(user);
+    if (!allowed) throw forbidden();
+
+    const isMember = isUser(user) && await controller.isMember(user.id, callId);
+    const eligible = isMember || isAdmin(req.user) || isGhost(user);
+    if (!eligible) throw forbidden();
+
+    // retrieve data from the database
+    const members = await controller.getCallMembers(callId);
+
+    // send response to the client
+    res.status(200).json({ members });
+  } catch(e) {
+    next(e);
+  }
 }
 
 export default {
   findCallById: asyncHandler(findCallById),
+  findCallMembers: asyncHandler(findCallMembers)
 };
