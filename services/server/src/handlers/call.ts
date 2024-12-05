2bdd74062dba8a9e2b011829dfbf4e3d2dad7c77
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-12-05 16:48:31 +0200

    feat(server): utilize calls cache in the wss handler

--- services/server/src/handlers/call.ts
+++ services/server/src/handlers/call.ts
@@ -1,9 +1,12 @@
 import { calls } from "@litespace/models";
 import { NextFunction, Request, Response } from "express";
-import asyncHandler from "express-async-handler";
+import safeRequest from "express-async-handler";
 import { withNamedId } from "@/validation/utils";
 import { forbidden, notfound } from "@/lib/error";
 import { isAdmin, isGhost, isUser } from "@litespace/auth";
+import { canJoinCall } from "@/lib/call";
+import { cache } from "@/lib/cache";
+import { ICall } from "@litespace/types";
 
 async function findCallById(req: Request, res: Response, next: NextFunction) {
   const user = req.user;
@@ -25,6 +28,32 @@ async function findCallById(req: Request, res: Response, next: NextFunction) {
   res.status(200).json({ call, members });
 }
 
+async function findCallMembers(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const user = req.user;
+  const allowed = isUser(user);
+  if (!allowed) return next(forbidden());
+
+  const { callId } = withNamedId("callId").parse(req.params);
+
+  const canJoin = await canJoinCall({
+    userId: user.id,
+    callId: callId,
+    callType: "lesson", // todo: accept from the request body
+  });
+  if (!canJoin) return next(forbidden());
+
+  const members = await cache.call.getMembers(callId);
+
+  const response: ICall.FindCAllMembersApiResponse = members;
+
+  res.status(200).json(response);
+}
+
 export default {
-  findCallById: asyncHandler(findCallById),
+  findCallById: safeRequest(findCallById),
+  findCallMembers: safeRequest(findCallMembers),
 };
