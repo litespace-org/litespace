b0d3bbeab35a5e6f3e9df137eacf3697eb234171
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-12-10 01:37:44 +0200

    feat(luna): add book lesson missing steps

--- packages/luna/src/components/Lessons/BookLesson/BookLesson.stories.tsx
+++ packages/luna/src/components/Lessons/BookLesson/BookLesson.stories.tsx
@@ -3,6 +3,7 @@ import { BookLesson } from "@/components/Lessons/BookLesson";
 import React from "react";
 import { identity } from "lodash";
 import { faker } from "@faker-js/faker/locale/ar";
+import { IRule } from "@litespace/types";
 
 type Component = typeof BookLesson;
 type Story = StoryObj<Component>;
@@ -22,11 +23,36 @@ const meta: Meta<Component> = {
   ],
 };
 
+const makeRule = (): IRule.Self => ({
+  id: faker.number.int(),
+  userId: faker.number.int(),
+  title: faker.lorem.words(),
+  frequency: IRule.Frequency.Daily,
+  start: faker.date.past().toISOString(),
+  end: faker.date.future().toISOString(),
+  duration: 8 * 60,
+  time: "10:00",
+  weekdays: [],
+  monthday: null,
+  activated: true,
+  deleted: false,
+  createAt: faker.date.past().toISOString(),
+  updatedAt: faker.date.past().toISOString(),
+});
+
 export const Primary: Story = {
   args: {
     open: true,
     close: identity,
+    tutorId: faker.number.int(),
     name: faker.person.fullName(),
+    imageUrl: faker.image.urlPicsumPhotos({ width: 400, height: 400 }),
+    rules: [makeRule()],
+    slots: [],
+    notice: 0,
+    onBook() {
+      alert("Lesson booked!!");
+    },
   },
 };
 
