4a5d13fb745d32bcbabb048d3f01086dc4eff5e7
mostafakamar2308 <moustafa.31011197@med.tanta.edu.eg>
2024-12-15 21:06:32 +0200

    feat(luna): added movable media reusable component

---
+++ packages/luna/src/components/MovableMedia/MovableMedia.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export const MovableMedia: React.FC<{
+  container: React.RefObject<HTMLDivElement>;
+  children: React.ReactNode;
+}> = ({ container, children }) => {
+  return (
+    <motion.div
+      drag
+      draggable
+      dragElastic={0}
+      dragConstraints={container}
+      whileDrag={{ scale: 1.05 }}
+      dragMomentum={false}
+    >
+      {children}
+    </motion.div>
+  );
+};
