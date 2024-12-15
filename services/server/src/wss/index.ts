25e97215f7f36e12c88696b3318a0228ea8d1bf7
M. E. Abdelsalam <mo.ehab150@gmail.com>
2024-12-15 16:16:14 +0200

    init: classes defined for managing wss connections.

--- services/server/src/wss/index.ts
+++ services/server/src/wss/index.ts
@@ -1,1 +1,18 @@
-export { wssHandler } from "@/wss/handler";
+import { Socket } from "socket.io";
+import { WSSHandlers } from "./handlers";
+import { logger } from "@litespace/sol";
+import { Wss } from "@litespace/types";
+
+
+const stdout = logger("wss");
+
+export function wssConnectionHandler(socket: Socket) {
+  const user = socket.request.user;
+  if (!user) {
+    stdout.warning("(function) wssHandler: No user has been found in the request obj!");
+    return;
+  }
+  const handlers = new WSSHandlers(socket, user);
+  socket.on(Wss.ClientEvent.JoinCall, handlers.call.onJoinCall);
+  socket.on(Wss.ClientEvent.LeaveCall, handlers.call.onLeaveCall);
+}
