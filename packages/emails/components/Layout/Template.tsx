c437c81876d085d192e5a2995605c92918b4635e
mohamed ali <elasasmohamed97@gmail.com>
2025-01-24 16:16:44 +0200

    feat(emails): impl forget password email

--- packages/emails/components/Layout/Template.tsx
+++ packages/emails/components/Layout/Template.tsx
@@ -2,9 +2,11 @@ import { Body, Font, Head, Html, Preview } from "@react-email/components";
 import React from "react";
 import Card from "@/components/Common/Card";
 import Tailwind from "@/components/Tailwind";
-import { translate } from "@/lib/translate";
 
-const Template: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+const Template: React.FC<{ preview: string; children?: React.ReactNode }> = ({
+  preview,
+  children,
+}) => {
   return (
     <Html lang="ar-eg" dir="rtl">
       <Head>
@@ -16,7 +18,7 @@ const Template: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
             format: "woff2",
           }}
         />
-        <Preview>{translate("verify-email.title")}</Preview>
+        <Preview>{preview}</Preview>
       </Head>
       <Tailwind>
         <Body
--- packages/emails/emails/ForgetPassword.tsx
+++ packages/emails/emails/ForgetPassword.tsx
@@ -1,11 +1,43 @@
+import Button from "@/components/Common/Button";
+import Typography from "@/components/Common/Typography";
+import Footer from "@/components/Layout/Footer";
+import Header from "@/components/Layout/Header";
+import Template from "@/components/Layout/Template";
+import { translate } from "@/lib/translate";
+import { Container, Section } from "@react-email/components";
 import * as React from "react";
-import { Html, Button } from "@react-email/components";
 
 export function ForgetPassword({ redirectUrl }: { redirectUrl: string }) {
   return (
-    <Html lang="en">
-      <Button href={redirectUrl}>Reset Password</Button>
-    </Html>
+    <Template preview={translate("forget-password-email.title")}>
+      <Header />
+
+      <Section className="mt-[46px] text-center">
+        <Typography element="h4" weight="bold" text="natural-950">
+          {translate("forget-password-email.title")}
+        </Typography>
+      </Section>
+
+      <Section className="w-[456px] mt-[24px] text-center">
+        <Typography element="body" weight="bold" text="natural-700">
+          {translate("forget-password-email.desc")}
+        </Typography>
+      </Section>
+
+      <Container>
+        <Section className="mt-[48px] text-center">
+          <Button href={redirectUrl}>
+            <Typography element="body" weight="bold" text="natural-50">
+              {translate("forget-password-email.confirm")}
+            </Typography>
+          </Button>
+        </Section>
+      </Container>
+
+      <Section className="mt-[64px]">
+        <Footer />
+      </Section>
+    </Template>
   );
 }
 
