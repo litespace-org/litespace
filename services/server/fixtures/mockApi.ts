9d95cf97c0a69ce31db93c4a9c9d255b69dbe1d4
Ahmed Ibrahim <me@ahmedibrahim.dev>
2025-01-27 15:56:38 +0200

    refactor(server): improve api mock testing logic

--- services/server/fixtures/mockApi.ts
+++ services/server/fixtures/mockApi.ts
@@ -1,90 +1,67 @@
 import Express from "express";
-import db from "@fixtures/db";
 import { IUser } from "@litespace/types";
+import db from "@fixtures/db";
 
-/**
- * This type shall be used instead of (as) Express.Request type.
- */
 type MockRequest<Body = object, Params = object, Query = object> = {
   body?: Body;
   params?: Params;
   query?: Query;
-  /**
-   * Specify the req.user attribute (logged in user)
-   */
-  user?: IUser.Self | IUser.Ghost;
-  /**
-   * In case the user attribute is undefined, this
-   * attribute is used while generating a mock user.
-   */
-  userRole?: IUser.Role;
+  user?: IUser.Self | IUser.Role;
 };
 
-/**
- * This type shall define (to be overriden) all Express.Response
- * methods that are used by the api handlers.
- */
-type MockResponse = {
-  sendStatus: (code: number) => MockResponse;
-  status: (code: number) => MockResponse;
-  json: (obj: object) => MockResponse;
-};
+class MockResponse<T> {
+  data: {
+    status: number | null;
+    body: T | null;
+  } = { status: null, body: null };
 
-/**
- * MockResponse type will probably only be used by this function,
- * as it does override Express.Response methods which should allow
- * us to mock Express.Handler functionality.
- * @param callback - This's mandatory to have access to the HTTP response payload.
- */
-function getMockResponse(callback?: (res: unknown) => void): Express.Response {
-  const mock: MockResponse = {
-    sendStatus: (res: number) => {
-      if (callback) callback(res);
-      return mock;
-    },
-    status: (res: number) => {
-      if (callback) callback(res);
-      return mock;
-    },
-    json: (res: object) => {
-      if (callback) callback(res);
-      return mock;
-    },
-  };
-  return mock as Express.Response;
+  sendStatus(status: number) {
+    this.data.status = status;
+    return this;
+  }
+
+  status(status: number) {
+    this.data.status = status;
+    return this;
+  }
+
+  json(body: T) {
+    this.data.body = body;
+    return this;
+  }
+
+  send(body: T) {
+    this.data.body = body;
+    return this;
+  }
 }
 
-/**
- * This function combines MockResponse and MockRequest together
- * to forge (return) a function that mocks Express.Handler functionality.
- * Generic types are used here to enable auto-complete, in case the user
- * wishes to.
- */
-export function mockApiHandler<
-  ReqBody = object,
-  ReqParams = object,
-  ReqQuery = object,
->(handler: Express.Handler) {
-  return async (request: MockRequest<ReqBody, ReqParams, ReqQuery>) => {
-    if (!request.user && request.userRole) {
-      request.user = await db.user({ role: request.userRole });
-    }
-    let response: unknown = undefined;
-    await asyncronize(
-      handler(
+export function mockApi<Body = object, Params = object, Query = object>(
+  handler: (
+    req: Express.Request,
+    res: Express.Response,
+    next: Express.NextFunction
+  ) => Promise<void> | void
+) {
+  return async <Response>(
+    request: MockRequest<Body, Params, Query>
+  ): Promise<{ status: number | null; body: Response | null }> => {
+    if (typeof request.user === "string")
+      request.user = await db.user({ role: request.user });
+
+    const response = new MockResponse<Response>();
+    return await new Promise((resolve) => {
+      const result = handler(
         request as unknown as Express.Request,
-        getMockResponse((res) => {
-          response = res;
-        }),
-        (err) => {
-          response = err;
-        }
-      )
-    );
-    return response;
-  };
-}
+        response as unknown as Express.Response,
+        (next) => resolve(next)
+      );
 
-async function asyncronize(func: unknown) {
-  return await func;
+      if (result instanceof Promise)
+        result.then(() => {
+          return resolve(response.data);
+        });
+      else return resolve(response.data);
+    });
+  };
 }
--- services/server/tests/handlers/topic.test.ts
+++ services/server/tests/handlers/topic.test.ts
@@ -1,29 +1,32 @@
 import { empty, forbidden, notfound, refused } from "@/lib/error";
-import { mockApiHandler } from "@fixtures/mockApi";
+import { mockApi } from "@fixtures/mockApi";
 import db, { faker } from "@fixtures/db";
 import { topics } from "@litespace/models";
 import { MAX_TOPICS_COUNT, safe } from "@litespace/sol";
 import { expect } from "chai";
 import { range } from "lodash";
-import topicHandlers from "@/handlers/topic";
-import { ITopic, IUser } from "@litespace/types";
+import handlers from "@/handlers/topic";
+import { EmptyObject, ITopic, IUser } from "@litespace/types";
+
+const addUserTopics = mockApi<ITopic.AddUserTopicsApiPayload>(
+  handlers.addUserTopics
+);
+
+const deleteUserTopics = mockApi<ITopic.DeleteUserTopicsApiPayload>(
+  handlers.deleteUserTopics
+);
+
+const findUserTopics = mockApi<
+  EmptyObject,
+  EmptyObject,
+  ITopic.FindTopicsApiQuery
+>(handlers.findUserTopics);
+
+const replaceUserTopics = mockApi<ITopic.ReplaceUserTopicsApiPayload>(
+  handlers.replaceUserTopics
+);
 
 describe("/api/v1/topic/", () => {
-  const addUserTopicsHandler = mockApiHandler<ITopic.AddUserTopicsApiPayload>(
-    topicHandlers.addUserTopics
-  );
-  const deleteUserTopicsHandler =
-    mockApiHandler<ITopic.DeleteUserTopicsApiPayload>(
-      topicHandlers.deleteUserTopics
-    );
-  const findUserTopicsHandler = mockApiHandler<ITopic.FindTopicsApiQuery>(
-    topicHandlers.findUserTopics
-  );
-  const replaceUserTopicsHandler =
-    mockApiHandler<ITopic.ReplaceUserTopicsApiPayload>(
-      topicHandlers.replaceUserTopics
-    );
-
   beforeEach(async () => {
     await db.flush();
   });
@@ -31,10 +34,7 @@ describe("/api/v1/topic/", () => {
   describe("POST /api/v1/topic/user", () => {
     it("should respond with 401 if the user neither a student, a tutor, nor a tutor-manager.", async () => {
       const res = await safe(() =>
-        addUserTopicsHandler({
-          body: { topicIds: [1] },
-          userRole: IUser.Role.SuperAdmin,
-        })
+        addUserTopics({ body: { topicIds: [1] }, user: IUser.Role.SuperAdmin })
       );
       expect(res).to.deep.eq(forbidden());
     });
@@ -54,9 +54,9 @@ describe("/api/v1/topic/", () => {
         )
       );
       const res = await safe(() =>
-        addUserTopicsHandler({
+        addUserTopics({
           body: { topicIds: mockTopicIds.slice(0, MAX_TOPICS_COUNT + 1) },
-          userRole: IUser.Role.Student,
+          user: IUser.Role.Student,
         })
       );
       expect(res).to.deep.eq(refused());
@@ -64,9 +64,9 @@ describe("/api/v1/topic/", () => {
 
     it("should respond with 404 if atleast one topic is not found.", async () => {
       const res = await safe(() =>
-        addUserTopicsHandler({
+        addUserTopics({
           body: { topicIds: [123] },
-          userRole: IUser.Role.Student,
+          user: IUser.Role.Student,
         })
       );
       expect(res).to.deep.eq(notfound.topic());
@@ -89,36 +89,31 @@ describe("/api/v1/topic/", () => {
         )
       );
 
-      const res1 = await safe(() =>
-        addUserTopicsHandler({
-          body: { topicIds: mockTopicIds },
-          user: student,
-        })
-      );
-      expect(res1).to.eq(200);
+      const res1 = await addUserTopics({
+        body: { topicIds: mockTopicIds },
+        user: student,
+      });
+      expect(res1.status).to.eq(200);
 
-      // it should ignore duplicated topics
-      const res2 = await safe(() =>
-        addUserTopicsHandler({
-          body: { topicIds: mockTopicIds },
-          user: student,
-        })
-      );
-      expect(res2).to.eq(200);
+      const res2 = await addUserTopics({
+        body: { topicIds: mockTopicIds },
+        user: student,
+      });
+      expect(res2.status).to.eq(200);
 
-      const myTopics = await topics.findUserTopics({
+      const userTopics = await topics.findUserTopics({
         users: [student.id],
       });
-      expect(myTopics.length).to.eq(3);
+      expect(userTopics.length).to.eq(3);
     });
   });
 
   describe("DELETE /api/v1/topic/user", () => {
     it("should respond with 401 if the user neither a student, a tutor, nor a tutor-manager.", async () => {
       const res = await safe(() =>
-        deleteUserTopicsHandler({
+        deleteUserTopics({
           body: { topicIds: [1] },
-          userRole: IUser.Role.SuperAdmin,
+          user: IUser.Role.SuperAdmin,
         })
       );
       expect(res).to.deep.eq(forbidden());
@@ -138,13 +133,11 @@ describe("/api/v1/topic/", () => {
             ).id
         )
       );
-      const res = await safe(() =>
-        deleteUserTopicsHandler({
-          body: { topicIds: mockTopicIds },
-          userRole: IUser.Role.Student,
-        })
-      );
-      expect(res).to.eq(200);
+      const res = await deleteUserTopics({
+        body: { topicIds: mockTopicIds },
+        user: IUser.Role.Student,
+      });
+      expect(res.status).to.eq(200);
     });
 
     it("should respond with 200 and successfully remove user topics.", async () => {
@@ -164,21 +157,17 @@ describe("/api/v1/topic/", () => {
         )
       );
 
-      const res1 = await safe(() =>
-        addUserTopicsHandler({
-          body: { topicIds: mockTopicIds },
-          user: student,
-        })
-      );
-      expect(res1).to.eq(200);
+      const res1 = await addUserTopics({
+        body: { topicIds: mockTopicIds },
+        user: student,
+      });
+      expect(res1.status).to.eq(200);
 
-      const res2 = await safe(() =>
-        deleteUserTopicsHandler({
-          body: { topicIds: mockTopicIds.slice(0, 2) },
-          user: student,
-        })
-      );
-      expect(res2).to.deep.eq(200);
+      const res2 = await deleteUserTopics({
+        body: { topicIds: mockTopicIds.slice(0, 2) },
+        user: student,
+      });
+      expect(res2.status).to.deep.eq(200);
 
       const myTopics = await topics.findUserTopics({
         users: [student.id],
@@ -190,7 +179,7 @@ describe("/api/v1/topic/", () => {
   describe("GET /api/v1/topic/user", () => {
     it("should respond with 401 if the user neither a student, a tutor, nor a tutor-manager.", async () => {
       const res = await safe(() =>
-        findUserTopicsHandler({ userRole: IUser.Role.SuperAdmin })
+        findUserTopics({ user: IUser.Role.SuperAdmin })
       );
       expect(res).to.deep.eq(forbidden());
     });
@@ -211,19 +200,16 @@ describe("/api/v1/topic/", () => {
       );
       const mockTopicIds = mockTopics.map((t) => t.id);
 
-      const res = await safe(() =>
-        addUserTopicsHandler({
-          body: { topicIds: mockTopicIds },
-          user: student,
-        })
-      );
-      expect(res).to.eq(200);
+      const res = await addUserTopics({
+        body: { topicIds: mockTopicIds },
+        user: student,
+      });
+      expect(res.status).to.eq(200);
 
-      const myTopics = await safe(async () =>
-        findUserTopicsHandler({ user: student })
-      );
-      expect(myTopics).to.be.an("array");
-      expect(myTopics).to.have.length(3);
+      const userTopics = await findUserTopics({ user: student });
+      console.log(userTopics);
+      expect(userTopics.body).to.be.an("array");
+      expect(userTopics.body).to.have.length(3);
     });
   });
 
@@ -250,16 +236,14 @@ describe("/api/v1/topic/", () => {
         topics: mockTopicIds.slice(0, 2),
       });
 
-      const res = await safe(() =>
-        replaceUserTopicsHandler({
-          body: {
-            removeTopics: mockTopicIds.slice(0, 2),
-            addTopics: mockTopicIds.slice(2, 4),
-          },
-          user: student,
-        })
-      );
-      expect(res).to.eq(200);
+      const res = await replaceUserTopics({
+        body: {
+          removeTopics: mockTopicIds.slice(0, 2),
+          addTopics: mockTopicIds.slice(2, 4),
+        },
+        user: student,
+      });
+      expect(res.status).to.eq(200);
     });
 
     it("should respond with 400 when the old and new list have different lengths.", async () => {
@@ -284,16 +268,14 @@ describe("/api/v1/topic/", () => {
         topics: mockTopicIds.slice(0, 2),
       });
 
-      const res = await safe(async () =>
-        replaceUserTopicsHandler({
-          body: {
-            removeTopics: mockTopicIds.slice(0, 2),
-            addTopics: mockTopicIds.slice(3, 4),
-          },
-          user: student,
-        })
-      );
-      expect(res).to.deep.eq(200);
+      const res = await replaceUserTopics({
+        body: {
+          removeTopics: mockTopicIds.slice(0, 2),
+          addTopics: mockTopicIds.slice(3, 4),
+        },
+        user: student,
+      });
+      expect(res.status).to.deep.eq(200);
     });
 
     it("should respond with 403 if the number of topics will exceed the MAX_TOPICS_NUM.", async () => {
@@ -319,7 +301,7 @@ describe("/api/v1/topic/", () => {
       });
 
       const res = await safe(async () =>
-        replaceUserTopicsHandler({
+        replaceUserTopics({
           body: {
             removeTopics: [],
             addTopics: mockTopicIds.slice(30, 31),
@@ -352,27 +334,23 @@ describe("/api/v1/topic/", () => {
         topics: mockTopicIds.slice(0, 2),
       });
 
-      const res1 = await safe(async () =>
-        replaceUserTopicsHandler({
-          body: {
-            removeTopics: [],
-            addTopics: mockTopicIds.slice(2, 4),
-          },
-          user: student,
-        })
-      );
-      expect(res1).to.eq(200);
+      const res1 = await replaceUserTopics({
+        body: {
+          removeTopics: [],
+          addTopics: mockTopicIds.slice(2, 4),
+        },
+        user: student,
+      });
+      expect(res1.status).to.eq(200);
 
-      const res2 = await safe(async () =>
-        replaceUserTopicsHandler({
-          body: {
-            removeTopics: mockTopicIds.slice(0, 2),
-            addTopics: [],
-          },
-          user: student,
-        })
-      );
-      expect(res2).to.eq(200);
+      const res2 = await replaceUserTopics({
+        body: {
+          removeTopics: mockTopicIds.slice(0, 2),
+          addTopics: [],
+        },
+        user: student,
+      });
+      expect(res2.status).to.eq(200);
     });
 
     it("should respond with 400 when both lists are empty.", async () => {
@@ -398,7 +376,7 @@ describe("/api/v1/topic/", () => {
       });
 
       const res = await safe(() =>
-        replaceUserTopicsHandler({
+        replaceUserTopics({
           body: {
             removeTopics: [],
             addTopics: [],
@@ -432,7 +410,7 @@ describe("/api/v1/topic/", () => {
       });
 
       const res = await safe(async () =>
-        replaceUserTopicsHandler({
+        replaceUserTopics({
           body: {
             removeTopics: [...mockTopicIds.slice(0, 2), 123],
             addTopics: [...mockTopicIds.slice(2, 4), 321],
@@ -466,7 +444,7 @@ describe("/api/v1/topic/", () => {
       });
 
       const res = await safe(async () =>
-        replaceUserTopicsHandler({
+        replaceUserTopics({
           body: {
             removeTopics: [...mockTopicIds.slice(0, 2)],
             addTopics: [...mockTopicIds.slice(2, 3), 123],
