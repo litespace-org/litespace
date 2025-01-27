0423a5188805492486b8964d753e53b8d7d16880
mostafakamar2308 <moustafa.31011197@med.tanta.edu.eg>
2025-01-27 18:20:37 +0200

    feat(luna): implemented error code map

--- packages/sol/src/error.ts
+++ packages/sol/src/error.ts
@@ -1,7 +1,25 @@
+import { ApiErrorCode } from "@litespace/types";
 export async function safe<T>(handler: () => Promise<T>) {
   try {
     return await handler();
   } catch (error) {
     return error instanceof Error ? error : new Error("unkown");
+  }
+}
+
+type CodedError = {
+  message: string;
+  code: ApiErrorCode;
+};
+
+// api error handling
+export class ResponseError extends Error {
+  statusCode: number;
+  errorCode: ApiErrorCode;
+
+  constructor(error: CodedError, statusCode: number) {
+    super(error.message);
+    this.statusCode = statusCode;
+    this.errorCode = error.code;
   }
 }
