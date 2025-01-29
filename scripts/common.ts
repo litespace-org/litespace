5c1076ef095366a4839979b67880df3fd82b8170
Ahmed Ibrahim <me@ahmedibrahim.dev>
2025-01-29 20:17:22 +0200

    chore: convert project name into all uppercase

--- scripts/common.ts
+++ scripts/common.ts
@@ -1,4 +1,4 @@
-import { capitalize } from "lodash";
+import { upperCase } from "lodash";
 
 export type Update = {
   update_id: number;
@@ -34,7 +34,7 @@ export function asVercelUrlsMessage(
   let message = prefix + "\n";
 
   for (const [name, url] of Object.entries(urls)) {
-    message += `🚀 [${capitalize(name)}](${asVercelUrl(url)}) \n`;
+    message += `🚀 [${upperCase(name)}](${asVercelUrl(url)}) \n`;
   }
 
   return message;
