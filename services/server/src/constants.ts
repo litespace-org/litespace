db1217b96d436bdb8440f07734c962caa56c8ebb
M. E. Abdelsalam <mo.ehab150@gmail.com>
2025-01-29 21:22:22 +0200

    added: digital ocean spaces config in server s3 lib file.

--- services/server/src/constants.ts
+++ services/server/src/constants.ts
@@ -95,6 +95,17 @@ export const emailConfig = {
     .parse(process.env.EMAIL_PASSWORD),
 } as const;
 
+export const spaceConfig = {
+  accessKeyId: zod
+    .string({ message: "Missing or invalid digital ocean space access key" })
+    .trim()
+    .parse(process.env.SPACES_ACCESS_KEY),
+  secretAccessKey: zod
+    .string({ message: "Missing or invalid digital ocean space secret key" })
+    .trim()
+    .parse(process.env.SPACES_SECRET_ACCESS_KEY),
+} as const;
+
 export const ghostConfig = {
   password: zod
     .string({
