a7c0c0f491d2fee7deb4e711750f41e98a3ffc52
Ahmed Ibrahim <me@ahmedibrahim.dev>
2025-02-11 15:56:55 +0200

    fix(ui): fix button focused state

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -60,14 +60,14 @@ export const Button: React.FC<{
       data-type={type}
       data-variant={variant}
       className={cn(
-        // general styles
+        // Teneral styles
         "tw-text-center tw-font-normal",
         "tw-flex tw-items-center tw-justify-center",
         "tw-relative tw-font-cairo tw-cursor-pointer",
         "tw-w-fit tw-flex tw-items-center tw-justify-center",
         "disabled:tw-opacity-50 disabled:tw-cursor-not-allowed",
         "tw-transition-colors tw-ease-out tw-duration-200 tw-rounded-lg tw-outline-none",
-        // bg color
+        // Background color
         {
           "hover:tw-bg-natural-100 active:tw-bg-natural-200": is.tertiary,
           "tw-bg-brand-700 hover:tw-bg-brand-600": is.main && is.primary,
@@ -88,7 +88,7 @@ export const Button: React.FC<{
           "tw-bg-neutral-50 hover:tw-bg-warning-50 active:tw-bg-warning-100":
             is.warning && is.secondary,
         },
-        // text color
+        // Text color
         {
           "tw-text-natural-50": is.primary,
           "tw-text-natural-700": is.tertiary,
@@ -99,22 +99,22 @@ export const Button: React.FC<{
             is.secondary && is.success,
           "tw-text-warning-700": is.warning && is.secondary,
         },
-        // font weight and size
+        // Font
         {
           "tw-font-medium tw-leading-[150%]": true,
           "tw-text-[0.875rem]": is.small,
           "tw-text-[1rem]": is.medium || is.large,
         },
-        // dimentions, margins, and paddings
+        // Spacing
         {
           "tw-px-2 tw-h-7": is.small,
           "tw-px-3 tw-h-8": is.medium,
           "tw-px-4 tw-h-10": is.large,
         },
-        // border
+        // Border & Focus
         // TODO: add `primary` variant borders.
         {
-          "tw-border focus-visible:tw-ring-[0.5px] focus-visible:tw-ring-natural-800 focus-visible:tw-border-natural-800":
+          "tw-border focus-visible:tw-outline-2 focus-visible:tw-outline-secondary-600 focus-visible:-tw-outline-offset-2":
             is.primary || is.secondary || is.tertiary,
           "tw-border-transparent hover:tw-border-natural-100 active:tw-border-natural-200":
             is.tertiary,
