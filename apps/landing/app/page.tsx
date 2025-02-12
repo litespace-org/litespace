cf09ea762811b3f2eb7bafbfda162fb848c5dc6b
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2025-02-13 00:04:13 +0200

    feat(landing): render hero section in main page

--- apps/landing/app/page.tsx
+++ apps/landing/app/page.tsx
@@ -1,13 +1,65 @@
+"use client";
+
+import { Button } from "@litespace/ui/Button";
+import { useFormatMessage } from "@litespace/ui/hooks/intl";
+import { Typography } from "@litespace/ui/Typography";
 import cn from "classnames";
+import Link from "next/link";
+
+// const HERO_SECTION_MIN_HEIGHT = 866;
 
 export default function Home() {
+  const intl = useFormatMessage();
   return (
-    <main
-      className={cn(
-        "flex min-h-screen flex-col items-center justify-between p-24"
-      )}
-    >
-      <h1 className="text-9xl italic">home page</h1>
+    <main className={cn("w-screen")}>
+      {/* hero section */}
+      <div className="relative flex flex-col items-center gap-12 pt-[293px] bg-brand-900 h-[866px] overflow-hidden">
+        {/* ellipses */}
+        <div className="absolute top-0 -left-[200px] rotate-[30deg] w-[600px] h-[430px] z-10 bg-circular-gradient-brand rounded-full overflow-hidden">
+          &nbsp;
+        </div>
+        <div className="absolute -bottom-[200px] -left-[200px] rotate-[30deg] w-[600px] h-[430px] z-10 bg-circular-gradient-brand rounded-full overflow-hidden">
+          &nbsp;
+        </div>
+        <div className="absolute -top-[200px] -right-[200px] rotate-[30deg] w-[600px] h-[430px] z-10 bg-circular-gradient-brand rounded-full overflow-hidden">
+          &nbsp;
+        </div>
+        <div className="absolute -bottom-[75px] -right-[150px] rotate-[30deg] w-[600px] h-[430px] z-10 bg-circular-gradient-brand-peripheral-transparent rounded-full overflow-hidden">
+          &nbsp;
+        </div>
+        <div className="absolute bottom-[175px] right-[75px] rotate-[30deg] w-[600px] h-[430px] z-[5] bg-circular-gradient-warning rounded-full overflow-hidden">
+          &nbsp;
+        </div>
+        <div className="absolute bottom-[75px] left-0 rotate-[30deg] w-[600px] h-[430px] z-[5] bg-circular-gradient-warning rounded-full overflow-hidden">
+          &nbsp;
+        </div>
+
+        <div className="absolute z-20 flex flex-col items-center text-center gap-12">
+          <div className="mx-auto flex flex-col items-center text-center gap-10 max-w-[808px]">
+            <Typography element="h2" weight="bold" className="text-natural-50">
+              {intl("landing.home.hero-section.title")}
+            </Typography>
+            <Typography
+              element="subtitle-1"
+              weight="medium"
+              className="text-natural-50"
+            >
+              {intl("landing.home.hero-section.description")}
+            </Typography>
+          </div>
+          <Link href="https://app.litespace.org">
+            <Button size="large">
+              <Typography
+                element="body"
+                weight="bold"
+                className="text-natural-50"
+              >
+                {intl("landing.home.hero-section.start-journey-btn")}
+              </Typography>
+            </Button>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
--- apps/landing/tailwind.config.ts
+++ apps/landing/tailwind.config.ts
@@ -12,5 +12,17 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    ...baseConfig.theme,
+    backgroundImage: {
+      ...baseConfig.theme?.backgroundImage,
+      "circular-gradient-brand":
+        "radial-gradient(ellipse, var(--brand-800) 0%, var(--brand-800) 5%, var(--brand-800) 10%, var(--brand-800) 15%, transparent 65%, var(--brand-900) 75%)",
+      "circular-gradient-brand-peripheral-transparent":
+        "radial-gradient(ellipse, var(--brand-800) 0%, var(--brand-800) 5%, var(--brand-800) 10%, var(--brand-800) 15%, transparent 65%, transparent 75%)",
+      "circular-gradient-warning":
+        "radial-gradient(ellipse, var(--warning-900) 0%, var(--warning-900) 1%, var(--warning-900) 2%, var(--warning-900) 3%, transparent 55%, var(--brand-900) 75%)",
+    },
+  },
 };
 export default config;
