4c4fc5b803c55cb8b1e7c29ecf76fad4e1d83c4f
mostafakamar2308 <moustafa.31011197@med.tanta.edu.eg>
2025-02-17 12:45:19 +0200

    feat(ui): implemented generic intl-hook-creator and removed non-shared labels

--- packages/ui/src/hooks/intl.ts
+++ packages/ui/src/hooks/intl.ts
@@ -1,24 +1,31 @@
-import { LocalMap, messages } from "@/locales";
 import React, { useCallback, useMemo } from "react";
 import { PrimitiveType, useIntl } from "react-intl";
+import uiMessages from "@/locales/ar-eg.json" assert { type: "json" };
 
-export function useFormatMessage() {
-  const intl = useIntl();
+export function createFormatMessageHook<T extends Record<string, string>>(customMessages?: T) {
+  return function useFormatMessage() {
+    const intl = useIntl();
 
-  const format = useCallback(
-    (id: keyof LocalMap, values?: Record<string, PrimitiveType>) =>
-      intl.formatMessage({ id: messages[id] }, values),
-    [intl]
-  );
+    const mergedMessages = useMemo(() => ({ ...uiMessages, ...customMessages }), []);
 
-  return useMemo(
-    () =>
-      Object.assign(format, {
-        node: (
-          id: keyof LocalMap,
-          values?: Record<string, PrimitiveType | React.ReactNode>
-        ): React.ReactNode => intl.formatMessage({ id: messages[id] }, values),
-      }),
-    [format, intl]
-  );
+    const format = useCallback(
+      (id: keyof typeof mergedMessages, values?: Record<string, PrimitiveType>) =>
+        intl.formatMessage({ id: mergedMessages[id] }, values),
+      [intl, mergedMessages]
+    );
+
+    return useMemo(
+      () =>
+        Object.assign(format, {
+          node: (
+            id: keyof typeof mergedMessages,
+            values?: Record<string, PrimitiveType | React.ReactNode>
+          ): React.ReactNode => intl.formatMessage({ id: mergedMessages[id] }, values),
+        }),
+      [format, intl, mergedMessages]
+    );
+  };
 }
+
+// The generic intl for the ui packages
+export const useFormatMessage = createFormatMessageHook()
