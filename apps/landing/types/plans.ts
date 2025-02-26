fcc009f7bbdfa2682837fb4295fd2c155f813b9e
mostafakamar2308 <moustafa.31011197@med.tanta.edu.eg>
2025-02-26 04:04:16 +0200

    feat(landing): added plans sections and plans page

---
+++ apps/landing/types/plans.ts
@@ -0,0 +1,46 @@
+import { Route } from "next";
+
+export type Tab = "monthly" | "quarter" | "half" | "annual";
+
+export type PlanCardProps = {
+  /**
+   * the title of the plan wheather
+   *  - beginner
+   *  - advanced
+   *  - professional
+   */
+  title: string;
+  /**
+   * sammary about the plan
+   */
+  description: string;
+  /**
+   * number of minutes per week
+   */
+  weeklyMinutes: number;
+  /**
+   * price in egp (unscale)
+   */
+  price: number;
+  label?: "most-common" | "most-valuable";
+  primary?: boolean;
+  /**
+   * floating number between 0 and 100.
+   */
+  href: Route;
+};
+
+export type PlanProps = {
+  id: number;
+  weeklyMinutes: number;
+  price: number;
+  label?: "most-common" | "most-valuable";
+  primary?: boolean;
+};
+
+export type PlansDataProps = {
+  monthly: PlanProps[];
+  quarter: PlanProps[];
+  half: PlanProps[];
+  annual: PlanProps[];
+};
