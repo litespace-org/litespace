94301da0d6c2e10547db7f51389937810391b7d4
mohamed ali <elasasmohamed97@gmail.com>
2025-03-12 00:56:35 +0200

    feat(web) render invoices table

--- apps/web/src/pages/Invoices.tsx
+++ apps/web/src/pages/Invoices.tsx
@@ -1,13 +1,15 @@
 import InvoicesOverview from "@/components/Invoices/InvoicesOverview";
 import { useUserContext } from "@litespace/headless/context/user";
+import { Table as InvoicesTable } from "@/components/Invoices/Table";
 import React from "react";
 
 const Invoices: React.FC = () => {
   const { user } = useUserContext();
 
   return (
-    <div className="flex flex-col lg:flex-row p-4 sm:p-6 gap-6 max-w-screen-3xl mx-auto w-full">
+    <div className="flex flex-col p-4 sm:p-6 gap-4 lg:gap-6 max-w-screen-3xl mx-auto w-full">
       <InvoicesOverview tutorId={user?.id} />
+      <InvoicesTable />
     </div>
   );
 };
