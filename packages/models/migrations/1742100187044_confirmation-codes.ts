c128712ac27853971baf154c551eab4268d49afd
mostafakamar2308 <moustafa.31011197@med.tanta.edu.eg>
2025-03-16 13:37:43 +0200

    feat(models): implemented confirmation codes to models

---
+++ packages/models/migrations/1742100187044_confirmation-codes.ts
@@ -0,0 +1,32 @@
+import { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  // tables
+  pgm.createTable("confirmation_codes", {
+    id: { type: "SERIAL", primaryKey: true, notNull: true },
+    code: { type: "smallint", notNull: true },
+    purpose: { type: "smallint", notNull: true },
+    created_at: {
+      type: "TIMESTAMPTZ",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+    expires_at: { type: "TIMESTAMPTZ", notNull: true },
+  });
+
+  // constraints
+  pgm.addConstraint("confirmation_codes", "unique_code_purpose", {
+    unique: ["code", "purpose"],
+  });
+
+  // indexes for fast lookups
+  pgm.createIndex("confirmation_codes", "code");
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropConstraint("confirmation_codes", "unique_code_purpose", {
+    ifExists: true,
+  });
+  pgm.dropIndex("confirmation_codes", "code", { ifExists: true });
+  pgm.dropTable("confirmation_codes", { ifExists: true, cascade: true });
+}
