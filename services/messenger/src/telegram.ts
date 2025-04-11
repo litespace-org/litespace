49b5e0ccb91262f390b1e9fc8c3054cc0b44bc93
Ahmed Ibrahim <me@ahmedibrahim.dev>
2025-04-11 05:15:10 +0200

    feat(messenger): resolve telegram users by phone number

--- services/messenger/src/telegram.ts
+++ services/messenger/src/telegram.ts
@@ -4,6 +4,8 @@ import { msg } from "@/lib/bot";
 import { Consumer } from "@litespace/kafka";
 import ms from "ms";
 import { safePromise } from "@litespace/utils";
+import { Api } from "telegram";
+// import { generateRandomBytes, readBigIntFromBuffer } from "telegram/Helpers";
 
 // Global error handling. This is needed to prevent the server process from
 // exit.
@@ -26,6 +28,45 @@ async function main() {
   });
   await telegram.start();
 
+  // const result = await telegram.client.invoke(
+  //   new Api.contacts.AddContact({
+  //     firstName: "Ahmed",
+  //     lastName: "Ibrahim",
+  //     phone: "+201552832217",
+  //   })
+  // );
+
+  // console.log(result);
+
+  // const result = await telegram.client.invoke(
+  //   new Api.contacts.ImportContacts({
+  //     contacts: [
+  //       new Api.InputPhoneContact({
+  //         clientId: readBigIntFromBuffer(generateRandomBytes(8)),
+  //         // phone: "+201552832217",
+  //         // firstName: "Ahmed",
+  //         // lastName: "Ibrahim",
+  //         phone: "+966544546735",
+  //         firstName: "Heba",
+  //         lastName: "Ibrahim",
+  //       }),
+  //     ],
+  //   })
+  // );
+
+  const result = await telegram.client.invoke(
+    new Api.contacts.ResolvePhone({
+      // phone: "+201018303125",
+      phone: "+966544546735",
+      // phone: "+201143759540",
+      // phone: "+201030070409",
+      // phone: "+201062818862",
+    })
+  );
+
+  const [user] = result.users;
+  const id = user.id;
+
   const consumer = new Consumer<"telegram">("telegram");
   await consumer.connect();
   await consumer.subscribe({ topics: ["telegram"], fromBeginning: true });
@@ -33,7 +74,7 @@ async function main() {
     async eachMessage({ topic, value }) {
       console.log(`[${topic}]: processing mesage`);
       if (!value) return;
-      const id = telegram.asPhoneNumber(value.to);
+      // const id = telegram.asPhoneNumber(value.to);
       const result = await safePromise(
         telegram.sendMessage(id, { message: value.message })
       );
