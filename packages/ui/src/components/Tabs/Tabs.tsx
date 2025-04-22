2fdcb4f112158eb074497b568ed2450371120368
Ahmed Ibrahim <me@ahmedibrahim.dev>
2025-04-22 10:02:43 +0200

    feat(ui): add type safety to the tabs component

--- packages/ui/src/components/Tabs/Tabs.tsx
+++ packages/ui/src/components/Tabs/Tabs.tsx
@@ -2,38 +2,42 @@ import React from "react";
 import { Typography } from "@/components/Typography";
 import cn from "classnames";
 
-export const Tabs: React.FC<{
+export const Tabs = <T extends string>({
+  tab,
+  tabs,
+  setTab,
+}: {
   /**
    * The Id of the current tab
    */
-  tab?: string;
-  tabs: Array<{ id: string; label: string; important?: boolean }>;
-  setTab: (id: string) => void;
-}> = ({ tab, tabs, setTab }) => {
+  tab?: T;
+  tabs: Array<{ id: T; label: string; important?: boolean }>;
+  setTab: (id: T) => void;
+}) => {
   return (
     <div className="border border-natural-200 rounded-xl flex items-center">
-      {tabs.map((t) => (
+      {tabs.map(({ id, label, important }) => (
         <button
-          key={t.id}
+          key={id}
           type="button"
           className={cn(
             "relative px-5 py-4 group rounded-xl",
             "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-offset-0 focus-visible:ring-secondary-600"
           )}
-          onClick={() => setTab(t.id)}
+          onClick={() => setTab(id)}
         >
           <Typography
             tag="h6"
             className={cn(
               "font-normal text-body ",
-              t.id === tab
+              id === tab
                 ? "text-brand-700"
                 : "text-natural-500 group-hover:text-brand-500 group-active:text-brand-700"
             )}
           >
-            {t.label}
+            {label}
           </Typography>
-          {t.important ? (
+          {important ? (
             <div className="absolute top-2 right-5 bg-destructive-500 w-2 h-2 rounded-full" />
           ) : null}
         </button>
