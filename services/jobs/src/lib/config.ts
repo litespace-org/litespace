eb449a228ca15fb873b78b6eb711577abd02e4dc
M. E. Abdelsalam <mo.ehab150@gmail.com>
2025-05-25 20:48:41 +0300

    impl(jobs): database backup automation initial implementation.

--- services/jobs/src/lib/config.ts
+++ services/jobs/src/lib/config.ts
@@ -11,6 +11,10 @@ const schema = zod.object({
     zod.literal("production"),
   ]),
   adminPhoneNumber: zod.string().length(11),
+  /**
+   * restore file path
+   */
+  rfp: zod.string(),
 });
 
 type ConfigSchema = Zod.infer<typeof schema>;
@@ -22,4 +26,20 @@ export const config: ConfigSchema = schema.parse({
   },
   env: process.env.ENVIRONMENT,
   adminPhoneNumber: process.env.ADMIN_PHONE_NUMBER,
+  rfp: process.env.RFP,
 });
+
+export const spaceConfig = {
+  bucketName: zod
+    .string({ message: "Missing or invalid digital ocean space bucket name" })
+    .trim()
+    .parse(process.env.SPACES_BUCKET_NAME),
+  accessKeyId: zod
+    .string({ message: "Missing or invalid digital ocean space access key" })
+    .trim()
+    .parse(process.env.SPACES_ACCESS_KEY),
+  secretAccessKey: zod
+    .string({ message: "Missing or invalid digital ocean space secret key" })
+    .trim()
+    .parse(process.env.SPACES_SECRET_ACCESS_KEY),
+} as const;
