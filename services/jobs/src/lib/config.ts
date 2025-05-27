ab3bcd1d3f41d41519ead7dd38aab298f9b25a58
M. E. Abdelsalam <mo.ehab150@gmail.com>
2025-05-27 14:44:55 +0300

    refactor: psql backup job has been more flexible by using env variables.

--- services/jobs/src/lib/config.ts
+++ services/jobs/src/lib/config.ts
@@ -14,6 +14,13 @@ const schema = zod.object({
    * restore file path
    */
   rfp: zod.string(),
+  /**
+   * @NOTE: backupMethod is a file extension; *.sql, etc.
+   */
+  backupMethod: zod
+    .literal("dump")
+    .or(zod.literal("sql"))
+    .or(zod.literal("tar")),
 });
 
 type ConfigSchema = Zod.infer<typeof schema>;
@@ -25,7 +32,13 @@ export const config: ConfigSchema = schema.parse({
   },
   env: process.env.ENVIRONMENT,
   rfp: process.env.RFP,
+  backupMethod: process.env.BACKUP_METHOD,
 });
+
+export const dbConfig = {
+  user: zod.string().trim().parse(process.env.PG_USER),
+  database: zod.string().trim().parse(process.env.PG_DATABASE),
+} as const;
 
 export const spaceConfig = {
   bucketName: zod
