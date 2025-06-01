d431f49a4d7a97cdf2b203004fef2b0cc97a0bb5
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2025-06-01 09:57:00 +0300

    feat(web): implemented subscription based lesson booking

--- packages/models/scripts/seed.ts
+++ packages/models/scripts/seed.ts
@@ -11,11 +11,15 @@ import {
   hashPassword,
   topics,
   availabilitySlots,
+  subscriptions,
+  transactions,
 } from "@litespace/models";
 import {
   IAvailabilitySlot,
   IInterview,
   ILesson,
+  IPlan,
+  ITransaction,
   IUser,
 } from "@litespace/types";
 import dayjs from "dayjs";
@@ -62,6 +66,37 @@ async function main(): Promise<void> {
     birthYear: birthYear(),
   });
 
+  // Creating Plans
+  const [plan1] = await Promise.all([
+    plans.create({
+      weeklyMinutes: 2.5 * 60,
+      forInvitesOnly: false,
+      baseMonthlyPrice: price.scale(2500),
+      monthDiscount: percentage.scale(10),
+      quarterDiscount: percentage.scale(20),
+      yearDiscount: percentage.scale(30),
+      active: true,
+    }),
+    plans.create({
+      weeklyMinutes: 5 * 60,
+      forInvitesOnly: false,
+      baseMonthlyPrice: price.scale(4000),
+      monthDiscount: percentage.scale(15),
+      quarterDiscount: percentage.scale(20),
+      yearDiscount: percentage.scale(30),
+      active: true,
+    }),
+    plans.create({
+      weeklyMinutes: 8 * 60,
+      forInvitesOnly: false,
+      baseMonthlyPrice: price.scale(6000),
+      monthDiscount: percentage.scale(20),
+      quarterDiscount: percentage.scale(25),
+      yearDiscount: percentage.scale(30),
+      active: true,
+    }),
+  ]);
+
   const student = await knex.transaction(async (tx) => {
     const student = await users.create(
       {
@@ -78,6 +113,7 @@ async function main(): Promise<void> {
     return student;
   });
 
+  // Creating full students with subscriptions
   const students = await Promise.all(
     range(20).map(
       async (idx) =>
@@ -112,6 +148,28 @@ async function main(): Promise<void> {
         })
     )
   );
+
+  students.forEach(async (student, idx) => {
+    const transaction = await transactions.create({
+      amount: 1200,
+      paymentMethod: ITransaction.PaymentMethod.EWallet,
+      planId: plan1.id,
+      planPeriod: IPlan.Period.Month,
+      providerRefNum: 1,
+      userId: student.id,
+      status: ITransaction.Status.Paid,
+    });
+
+    await subscriptions.create({
+      userId: student.id,
+      planId: plan1.id,
+      start: dayjs.utc().toISOString(),
+      end: dayjs.utc().add(1, "day").toISOString(),
+      period: IPlan.Period.Month,
+      weeklyMinutes: idx * 10,
+      txId: transaction.id,
+    });
+  });
 
   // seeding studios
   const studio1 = await users.create({
@@ -239,7 +297,8 @@ async function main(): Promise<void> {
   if (!tutor) throw new Error("Tutor not found; should never happen.");
 
   const tutorManager = first(addedTutorManagers);
-  if (!tutorManager)
+  const tutorManager2 = addedTutorManagers[1];
+  if (!tutorManager || !tutorManager2)
     throw new Error("TutorManager not found; should never happen.");
 
   // seeding ratings data
@@ -286,11 +345,9 @@ async function main(): Promise<void> {
 
   // seeding slots
   const seededSlots: { [tutorId: number]: IAvailabilitySlot.Self[] } = {};
-  addedTutors.forEach(async (tutor, i) => {
-    const date = dayjs
-      .utc()
-      .add(i * 4, "days")
-      .startOf("day");
+  [...addedTutors, tutorManager].forEach(async (tutor, i) => {
+    // return this to i instead of 1
+    const date = dayjs.utc().add(1, "days").startOf("day");
     const slots = await availabilitySlots.create([
       {
         userId: tutor.id,
@@ -394,7 +451,7 @@ async function main(): Promise<void> {
   const slot = (
     await availabilitySlots.create([
       {
-        userId: tutorManager.id,
+        userId: tutorManager2.id,
         start: dayjs.utc().startOf("day").toISOString(),
         end: dayjs.utc().startOf("day").add(1, "days").toISOString(),
       },
@@ -406,7 +463,7 @@ async function main(): Promise<void> {
       const interview = await interviews.create({
         session: `interview:${randomUUID()}`,
         interviewee: tutor.id,
-        interviewer: tutorManager.id,
+        interviewer: tutorManager2.id,
         start: randomStart(),
         slot: slot.id,
         tx,
@@ -428,36 +485,6 @@ async function main(): Promise<void> {
       );
     });
   }
-
-  await Promise.all([
-    plans.create({
-      weeklyMinutes: 2.5 * 60,
-      forInvitesOnly: false,
-      baseMonthlyPrice: price.scale(2500),
-      monthDiscount: percentage.scale(10),
-      quarterDiscount: percentage.scale(20),
-      yearDiscount: percentage.scale(30),
-      active: true,
-    }),
-    plans.create({
-      weeklyMinutes: 5 * 60,
-      forInvitesOnly: false,
-      baseMonthlyPrice: price.scale(4000),
-      monthDiscount: percentage.scale(15),
-      quarterDiscount: percentage.scale(20),
-      yearDiscount: percentage.scale(30),
-      active: true,
-    }),
-    plans.create({
-      weeklyMinutes: 8 * 60,
-      forInvitesOnly: false,
-      baseMonthlyPrice: price.scale(6000),
-      monthDiscount: percentage.scale(20),
-      quarterDiscount: percentage.scale(25),
-      yearDiscount: percentage.scale(30),
-      active: true,
-    }),
-  ]);
 
   await knex.transaction(async (tx) => {
     const roomId = await rooms.create([tutor.id, tutorManager.id], tx);
--- packages/ui/src/components/Lessons/ManageLesson/TimeSelection.tsx
+++ packages/ui/src/components/Lessons/ManageLesson/TimeSelection.tsx
@@ -12,42 +12,44 @@ export const TimeSelection: React.FC<{
   select: (slotId: number, start: string) => void;
 }> = ({ slots, start, slotId, select }) => {
   return (
-    <div className="lg:px-8">
-      <div
-        className={cn(
-          "grid grid-cols-[repeat(auto-fill,minmax(60px,1fr))] gap-2 md:gap-4 max-h-96 overflow-y-auto",
-          "scrollbar-thin scrollbar-thumb-neutral-200 scrollbar-track-transparent px-4 md:px-6 pt-3 lg:pt-4 pb-5 md:mb-3"
-        )}
-      >
-        {slots.map((slot) => {
-          return (
-            <motion.button
-              whileHover={{ scale: slot.bookable ? 1.05 : 1 }}
-              whileTap={{ scale: slot.bookable ? 0.95 : 1 }}
-              type="button"
-              key={slot.start}
-              disabled={!slot.bookable}
-              onClick={() => select(slot.parent, slot.start)}
-              data-selected={slot.start === start && slot.parent === slotId}
+    <div
+      className={cn(
+        "flex flex-wrap gap-2 md:gap-x-3 md:gap-y-[10px] max-h-[350px] overflow-y-auto",
+        "scrollbar-thin scrollbar-thumb-neutral-200 scrollbar-track-transparent mt-8 mb-6"
+      )}
+    >
+      {slots.map((slot) => {
+        const selected = slot.start === start && slot.parent === slotId;
+        return (
+          <motion.button
+            whileHover={{ scale: slot.bookable ? 1.05 : 1 }}
+            whileTap={{ scale: slot.bookable ? 0.95 : 1 }}
+            type="button"
+            key={slot.start}
+            disabled={!slot.bookable}
+            onClick={() => select(slot.parent, slot.start)}
+            className={cn(
+              "border shadow-time-selection-item transition-colors duration-300",
+              "h-20 w-20 rounded-lg flex items-center justify-center",
+              selected
+                ? "bg-brand-700 border-brand-800"
+                : "border-natural-200 hover:bg-brand-100 hover:border-natural-300 bg-natural-50",
+              "disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-natural-50"
+            )}
+          >
+            <Typography
+              tag="span"
+              data-selected={slot.start === start}
               className={cn(
-                "bg-natural-50 border border-natural-800 shadow-time-selection-item",
-                "mx-auto h-12 w-[59px] md:h-[60px] md:w-[60px] rounded-lg flex items-center justify-center",
-                "data-[selected=false]:hover:bg-brand-100 transition-colors duration-300",
-                "data-[selected=true]:bg-brand-700 data-[selected=true]:border-brand-800",
-                "disabled:opacity-20 disabled:cursor-not-allowed disabled:hover:bg-natural-50"
+                "transition-colors duration-300 text-tiny",
+                selected ? "text-natural-50" : "text-natural-700"
               )}
             >
-              <Typography
-                tag="span"
-                data-selected={slot.start === start}
-                className="text-natural-950 data-[selected=true]:text-natural-50 transition-colors duration-300 text-tiny"
-              >
-                {dayjs(slot.start).format("hh:mm a")}
-              </Typography>
-            </motion.button>
-          );
-        })}
-      </div>
+              {dayjs(slot.start).format("hh:mm a")}
+            </Typography>
+          </motion.button>
+        );
+      })}
     </div>
   );
 };
