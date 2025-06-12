5c8043759cdbdba585964dd98446ad9f6c90caef
galalqassas0 <galalqassas0@gmail.com>
2025-06-12 11:00:16 +0300

    Add comprehensive Cypress e2e tests for registration form validation

--- apps/web/cypress/e2e/login.cy.ts
+++ apps/web/cypress/e2e/login.cy.ts
@@ -1,60 +1,58 @@
 import { Web } from "@litespace/utils/routes";
 
 describe("login", () => {
+  beforeEach(() => {
+    cy.visit(Web.Login);
+    cy.get("input[id=email]").clear();
+    cy.get("input[id=password]").clear();
+  });
   it("should login as a student", () => {
-    cy.visit(Web.Login);
-    cy.get("input[id=email]").clear().type("student-1@litespace.org");
-    cy.get("input[id=password]").clear().type("Password@8");
+    cy.get("input[id=email]").type("student-1@litespace.org");
+    cy.get("input[id=password]").type("Password@8");
     cy.get("form").submit();
   });
-
   it("should handle login with empty email and password", () => {
-    cy.visit(Web.Login);
-    cy.get("form").submit();
     
-    cy.url().should("include", "/login");
+    cy.get("form").submit();
+    cy.url().should("include", Web.Login);
+    cy.get("input[id=email]").should("have.attr", "data-state", "error");
+    cy.get("input[id=password]").should("have.attr", "data-state", "error");
   });
-
   it("should handle login with empty email", () => {
-    cy.visit(Web.Login);
-    cy.get("input[id=password]").clear().type("Password@8");
+    cy.get("input[id=password]").type("Password@8");
     cy.get("form").submit();
-    
-    cy.url().should("include", "/login");
+
+    cy.url().should("include", Web.Login);
+    cy.get("input[id=email]").should("have.attr", "data-state", "error");
   });
 
   it("should handle login with empty password", () => {
-    cy.visit(Web.Login);
-    cy.get("input[id=email]").clear().type("student-1@litespace.org");
+    cy.get("input[id=email]").type("student-1@litespace.org");
     cy.get("form").submit();
-    
-    cy.url().should("include", "/login");
-  });
 
-  it("should handle login with non-existent user", () => {
-    cy.visit(Web.Login);
-    cy.get("input[id=email]").clear().type("galal@email");
-    cy.get("input[id=password]").clear().type("ValidPassword@123");
+    cy.url().should("include", Web.Login);
+    cy.get("input[id=password]").should("have.attr", "data-state", "error");
+  });  it("should handle login with non-existent user", () => {
+    cy.get("input[id=email]").type("galal@email");
+    cy.get("input[id=password]").type("ValidPassword@123");
     cy.get("form").submit();
-    
-    cy.url().should("include", "/login");
-  });
 
+    cy.url().should("include", Web.Login);
+    cy.get('[data-state="open"]').should("be.visible");
+  });
   it("should handle login with incorrect password", () => {
-    cy.visit(Web.Login);
-    cy.get("input[id=email]").clear().type("student-1@litespace.org");
-    cy.get("input[id=password]").clear().type("WrongPassword@123");
+    cy.get("input[id=email]").type("student-1@litespace.org");
+    cy.get("input[id=password]").type("WrongPassword@123");
     cy.get("form").submit();
-    
-    cy.url().should("include", "/login");
-  });
 
+    cy.url().should("include", Web.Login);
+    cy.get('[data-state="open"]').should("be.visible");
+  });
   it("should handle login with invalid email format", () => {
-    cy.visit(Web.Login);
-    cy.get("input[id=email]").clear().type("invalid-email");
-    cy.get("input[id=password]").clear().type("Password@8");
+    cy.get("input[id=email]").type("invalid-email");
+    cy.get("input[id=password]").type("Password@8");
     cy.get("form").submit();
-    
-    cy.url().should("include", "/login");
+
+    cy.url().should("include", Web.Login);
   });
 });
