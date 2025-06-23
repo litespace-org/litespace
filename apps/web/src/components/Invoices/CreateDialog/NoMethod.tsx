8edf55e96baf3369279a91daa588832aa2a44dd4
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2025-06-24 02:33:12 +0300

    feat(web): impl create invoice dialog

---
+++ apps/web/src/components/Invoices/CreateDialog/NoMethod.tsx
@@ -0,0 +1,58 @@
+import { Actions, AmountAndMethod } from "@/components/Invoices";
+import { useForm } from "@litespace/headless/form";
+import { IInvoice, Void } from "@litespace/types";
+import { useMakeValidators } from "@litespace/ui/hooks/validation";
+import { validateInvoiceAmount } from "@litespace/ui/lib/validate";
+import React from "react";
+
+type FormProps = {
+  amount: number;
+  method: IInvoice.WithdrawMethod | null;
+};
+
+export const NoMethod: React.FC<{
+  onClose: Void;
+  setAmount: (val: number) => void;
+  setMethod: (val: IInvoice.WithdrawMethod) => void;
+}> = ({ onClose, setAmount, setMethod }) => {
+  const validators = useMakeValidators<FormProps>({
+    amount: {
+      required: true,
+      validate: validateInvoiceAmount,
+    },
+    method: {
+      required: true,
+    },
+  });
+
+  const form = useForm<FormProps>({
+    defaults: {
+      amount: 0,
+      method: null,
+    },
+    validators,
+    onSubmit() {
+      // just for triggering validation
+    },
+  });
+
+  return (
+    <div className="flex flex-col gap-6 justify-center">
+      <AmountAndMethod
+        setAmount={(val) => {
+          form.set("amount", val);
+          setAmount(val);
+        }}
+        setMethod={setMethod}
+        amount={form.state.amount}
+        amountHelper={form.errors.amount}
+        amountState={form.errors.amount ? "error" : undefined}
+        methodState={form.errors.method ? "error" : undefined}
+        methodHelper={form.errors.method}
+      />
+      <Actions close={onClose} submit={form.submit} />
+    </div>
+  );
+};
+
+export default NoMethod;
