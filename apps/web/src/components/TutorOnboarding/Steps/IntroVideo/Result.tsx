a20d7ae5a8081600cdd5bfba4b216131c2747dd8
moalidv <elasasmohamed97@gmail.com>
2025-07-02 07:13:16 +0300

    update(web): update intro video design

--- apps/web/src/components/TutorOnboarding/Steps/IntroVideo/Result.tsx
+++ apps/web/src/components/TutorOnboarding/Steps/IntroVideo/Result.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { LogoHeader } from "@/components/TutorOnboarding/Steps/IntroVideo/Record";
+import { Typography } from "@litespace/ui/Typography";
+import { useFormatMessage } from "@litespace/ui/hooks/intl";
 
 export const Result: React.FC = () => {
-  return <div>Pending</div>;
+  const intl = useFormatMessage();
+
+  return (
+    <div>
+      <div className="_flex-1 flex flex-col gap-10 items-center mt-[60px] w-[681px] mx-auto">
+        <LogoHeader />
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-2">
+            <Typography tag="h3" className="text-h3 font-bold text-natural-950">
+              {intl("tutor-onboarding.steps.intro-video.title")}
+            </Typography>
+            <div className="flex flex-col gap-4">
+              <Typography
+                tag="p"
+                className="text-body font-normal text-natural-700"
+              >
+                {intl("tutor-onboarding.steps.intro-video.rejected.desc")}
+              </Typography>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Result;
--- apps/web/src/lib/dayjs.ts
+++ apps/web/src/lib/dayjs.ts
@@ -3,6 +3,7 @@ import isBetween from "dayjs/plugin/isBetween";
 import relative from "dayjs/plugin/relativeTime";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
+import duration from "dayjs/plugin/duration";
 import "dayjs/locale/ar";
 
 dayjs.locale("ar");
@@ -10,6 +11,7 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(relative);
 dayjs.extend(isBetween);
+dayjs.extend(duration);
 
 export { dayjs };
 export default dayjs;
