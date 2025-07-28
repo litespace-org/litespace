931e59baa27e4a3aab4752ab26d0f49ef416c46a
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2025-07-28 15:51:10 +0300

    refactor(ui): impl media variant for dialog

--- packages/ui/src/components/Dialog/Dialog.stories.tsx
+++ packages/ui/src/components/Dialog/Dialog.stories.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { DarkStoryWrapper } from "@/internal/DarkWrapper";
 import { Typography } from "@/components/Typography";
 import { faker } from "@faker-js/faker/locale/ar";
+import { VideoPlayer } from "@/components/VideoPlayer/V2";
 
 type Component = typeof Dialog;
 type Story = StoryObj<Component>;
@@ -33,7 +34,7 @@ export const Primary: Story = {
   args: {
     open: true,
     className: "w-[600px]",
-    close: () => {},
+    close: () => alert("closing..."),
   },
 };
 
@@ -42,7 +43,52 @@ export const PositionedAtBottom: Story = {
     open: true,
     className: "w-full",
     position: "bottom",
-    close: () => {},
+    close: () => alert("closing..."),
+  },
+};
+
+export const MediaLoading: Story = {
+  args: {
+    open: true,
+    variant: "media",
+    mediaLoading: true,
+    children: (
+      <VideoPlayer src="https://files.vidstack.io/sprite-fight/720p.mp4" />
+    ),
+    close: () => alert("closing..."),
+  },
+};
+
+export const MediaError: Story = {
+  args: {
+    open: true,
+    variant: "media",
+    mediaError: true,
+    mediaRefetch: () => alert("media refetching..."),
+    children: (
+      <VideoPlayer src="https://files.vidstack.io/sprite-fight/720p.mp4" />
+    ),
+    close: () => alert("closing..."),
+  },
+};
+
+export const VideoDialog: Story = {
+  args: {
+    open: true,
+    variant: "media",
+    children: (
+      <VideoPlayer src="https://files.vidstack.io/sprite-fight/720p.mp4" />
+    ),
+    close: () => alert("closing..."),
+  },
+};
+
+export const ImageDialog: Story = {
+  args: {
+    open: true,
+    variant: "media",
+    children: <img src="https://picsum.photos/400/600" />,
+    close: () => alert("closing..."),
   },
 };
 
