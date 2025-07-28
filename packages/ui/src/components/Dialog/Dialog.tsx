6b496548eeedcc9fcf6b42260e947057f897108c
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2025-07-28 15:51:10 +0300

    refactor(ui): impl media variant for dialog

--- packages/ui/src/components/Dialog/Dialog.tsx
+++ packages/ui/src/components/Dialog/Dialog.tsx
@@ -7,10 +7,12 @@ import {
   Portal,
 } from "@radix-ui/react-dialog";
 import cn from "classnames";
-import React, { useRef } from "react";
+import React, { useMemo, useRef } from "react";
 import X from "@litespace/assets/X";
 import { Void } from "@litespace/types";
 import { Optional } from "@/components/Optional";
+import { Button } from "@/components/Button";
+import CloseIcon from "@litespace/assets/Close";
 
 export const Dialog: React.FC<{
   container?: Element | null;
@@ -21,6 +23,7 @@ export const Dialog: React.FC<{
   description?: string;
   position?: "center" | "bottom";
   headless?: boolean;
+  variant?: "default" | "media";
   setOpen?: (open: boolean) => void;
   close?: Void;
 }> = ({
@@ -31,10 +34,16 @@ export const Dialog: React.FC<{
   description,
   position = "center",
   headless = false,
+  variant = "default",
   setOpen,
   close,
 }) => {
   const dialogRef = useRef<HTMLDivElement>(null);
+
+  const hiddenTitle = useMemo(() => {
+    if (variant === "media" || headless) return true;
+    return false;
+  }, [headless, variant]);
 
   return (
     <Root open={open} onOpenChange={setOpen}>
@@ -55,28 +64,65 @@ export const Dialog: React.FC<{
             const ye = y + height; // dialog y-end
             // the click event is considered within the dialog in case
             // "xc" is between [xs, xe] and "yx" is between [ys, ye]
-            const within = xc >= xs && xc <= xe && yc >= ys && yc <= ye;
-            if (!within) close();
+            const defaultWithin =
+              variant === "default" &&
+              xc >= xs &&
+              xc <= xe &&
+              yc >= ys &&
+              yc <= ye;
+
+            const mediaWithin =
+              (variant === "media" &&
+                xc >= xs &&
+                xc <= xe &&
+                yc >= ys &&
+                yc <= ye) ||
+              (variant === "media" && yc < 72);
+
+            if (!defaultWithin && !mediaWithin) close();
           }}
           className="fixed inset-0 backdrop-blur-[15px] bg-overlay-dialog z-dialog-overlay"
-        />
+        >
+          {variant === "media" ? (
+            <div className="px-6 absolute top-0 left-0 right-0 z-50 h-[72px] bg-natural-100 flex items-center justify-end">
+              <Button
+                size="large"
+                variant="secondary"
+                type="natural"
+                className="!bg-natural-100 hover:!bg-natural-200 active:!bg-natural-300 !border-natural-700"
+                startIcon={
+                  <CloseIcon className="[&>*]:stroke-natural-700 icon w-4 h-4" />
+                }
+                onClick={close}
+              />
+            </div>
+          ) : null}
+        </Overlay>
         <Content
           ref={dialogRef}
           dir="rtl"
           aria-describedby={description}
           className={cn(
-            "fixed bg-natural-50 focus-visible:ring-brand-500",
-            "p-4 sm:p-6 z-dialog",
+            "fixed bg-natural-50 focus-visible:ring-brand-500 overflow-hidden",
+            "z-dialog",
             {
-              "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-[32px]":
+              "p-4 sm:p-6": variant === "default",
+              "outline-none": variant === "media",
+            },
+            {
+              "rounded-[32px]": variant === "default" && position === "center",
+              "rounded-t-[24px]": position === "bottom",
+              "rounded-lg": variant === "media",
+            },
+            {
+              "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 focus-visible:ring-4 focus-visible:ring-brand-500":
                 position === "center",
-              "left-1/2 bottom-0 -translate-x-1/2 rounded-t-[24px]":
-                position === "bottom",
+              "left-1/2 bottom-0 -translate-x-1/2": position === "bottom",
             },
             className
           )}
         >
-          <Optional show={!headless}>
+          <Optional show={!hiddenTitle}>
             <div className="flex justify-between items-center w-full">
               <Title>{title}</Title>
               <Optional show={!!close}>
--- packages/ui/src/components/Dialog/index.ts
+++ packages/ui/src/components/Dialog/index.ts
@@ -1,2 +1,1 @@
 export { Dialog } from "@/components/Dialog/Dialog";
-export { VideoDialog } from "@/components/Dialog/VideoDialog";
