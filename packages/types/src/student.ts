20bf625c5a4ca80126b8376d196d266371c44623
galalqassas0 <galalqassas0@gmail.com>
2025-07-31 16:35:13 +0300

    add(db): implement students table migration & types

---
+++ packages/types/src/student.ts
@@ -0,0 +1,31 @@
+export enum EnglishLevel {
+  Beginner = 1,
+  PreIntermediate = 2,
+  Intermediate = 3,
+  UpperIntermediate = 4,
+  Advanced = 5,
+  Mastery = 6,
+}
+
+export type Self = {
+  id: number;
+  userId: number;
+  jobTitle: string | null;
+  englishLevel: EnglishLevel | null;
+  learningObjective: string | null;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type Row = {
+  id: number;
+  user_id: number;
+  job_title: string | null;
+  english_level: EnglishLevel | null;
+  learning_objective: string | null;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export type Column = keyof Row;
+export type Field = keyof Self;
