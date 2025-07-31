c573d80db74830655d9dd19932d292765cf79533
galalqassas0 <galalqassas0@gmail.com>
2025-07-31 16:35:13 +0300

    add(db): create students table

---
+++ packages/models/migrations/1753962774598_create-students-table.ts
@@ -0,0 +1,7 @@
+import { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable("students");
+}
