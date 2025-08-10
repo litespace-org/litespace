75ee0db8b1e204b0a98e0e090ecc03be5e673074
M. E. Abdelsalam <mo.ehab150@gmail.com>
2025-08-10 16:20:08 +0300

    fix: minor error in getting future slots;

---
+++ apps/web/src/components/Lessons/SubscriptionDialog.tsx
@@ -0,0 +1,54 @@
+import { router } from "@/lib/routes";
+import CalendarV2 from "@litespace/assets/CalendarV2";
+import { Void } from "@litespace/types";
+import { Button } from "@litespace/ui/Button";
+import { Dialog } from "@litespace/ui/Dialog";
+import { useFormatMessage } from "@litespace/ui/hooks/intl";
+import { Typography } from "@litespace/ui/Typography";
+import { Web } from "@litespace/utils/routes";
+import { Link } from "react-router-dom";
+
+export const SubscriptionDialog: React.FC<{
+  open: boolean;
+  close: Void;
+}> = ({ open, close }) => {
+  const intl = useFormatMessage();
+
+  return (
+    <Dialog
+      className="max-w-[400px]"
+      open={open}
+      close={close}
+      title={
+        <div className="w-12 h-12 bg-brand-50 rounded-full flex items-center justify-center">
+          <div className="bg-brand-100 rounded-full flex items-center justify-center p-1">
+            <CalendarV2 className="[&>*]:stroke-brand-500 w-6 h-6" />
+          </div>
+        </div>
+      }
+    >
+      <div className="flex flex-col gap-1 mt-4 mb-6">
+        <Typography
+          tag="p"
+          className="text-body font-semibold text-natural-950"
+        >
+          {intl("book-lesson.subscription-dialog.title")}
+        </Typography>
+
+        <Typography
+          tag="p"
+          className="text-caption font-regular text-natural-700"
+        >
+          {intl("book-lesson.subscription-dialog.body")}
+        </Typography>
+      </div>
+      <Link to={router.web({ route: Web.Plans, full: true })}>
+        <Button size="large" className="text w-full">
+          {intl("book-lesson.subscription-dialog.btn")}
+        </Button>
+      </Link>
+    </Dialog>
+  );
+};
+
+export default SubscriptionDialog;
