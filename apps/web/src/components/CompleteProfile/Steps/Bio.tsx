bd97219df6fb08296e020861b501d8f1f98431ed
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2025-08-19 08:45:11 +0300

    update(web): impl steps in complete profile

---
+++ apps/web/src/components/CompleteProfile/Steps/Bio.tsx
@@ -0,0 +1,181 @@
+import { governorates } from "@/constants/user";
+import { useOnError } from "@/hooks/error";
+import { QueryKey } from "@litespace/headless/constants";
+import { useUser } from "@litespace/headless/context/user";
+import { useForm } from "@litespace/headless/form";
+import { useInvalidateQuery } from "@litespace/headless/query";
+import { useUpdateUser } from "@litespace/headless/user";
+import { IUser, Void } from "@litespace/types";
+import { Button } from "@litespace/ui/Button";
+import { Form } from "@litespace/ui/Form";
+import { useFormatMessage } from "@litespace/ui/hooks/intl";
+import { useMakeValidators } from "@litespace/ui/hooks/validation";
+import { Input, Password } from "@litespace/ui/Input";
+import {
+  validatePassword,
+  validatePhone,
+  validateUserName,
+} from "@litespace/ui/lib/validate";
+import { PatternInput } from "@litespace/ui/PatternInput";
+import { Select } from "@litespace/ui/Select";
+import { useToast } from "@litespace/ui/Toast";
+import { getNullableFiledUpdatedValue } from "@litespace/utils";
+import { Web } from "@litespace/utils/routes";
+import React, { useCallback, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+
+type IForm = {
+  career: string;
+  level?: string;
+  aim: string;
+};
+
+const Bio: React.FC<{ next: Void }> = ({ next }) => {
+  const { user } = useUser();
+
+  const navigate = useNavigate();
+  const intl = useFormatMessage();
+
+  const toast = useToast();
+  const invalidateQuery = useInvalidateQuery();
+
+  // ============= Complete Profile Mutation ==============
+  const goRoot = useCallback(() => navigate(Web.Root), [navigate]);
+
+  const cityOptions = useMemo(
+    () =>
+      Object.entries(governorates).map(([key, value]) => ({
+        label: intl(value),
+        value: Number(key),
+      })),
+    [intl]
+  );
+
+  const onSuccess = useCallback(() => {
+    invalidateQuery([QueryKey.FindCurrentUser]);
+    next();
+  }, [invalidateQuery, next]);
+
+  const onError = useOnError({
+    type: "mutation",
+    handler: ({ messageId }) => {
+      toast.error({
+        title: intl("complete-profile.update.error"),
+        description: intl(messageId),
+      });
+    },
+  });
+
+  const updateUser = useUpdateUser({ onSuccess, onError });
+
+  // ============= Form ==============
+  const validators = useMakeValidators<IForm>({
+    career: { validate: validateUserName },
+    level: { validate: validatePhone },
+    aim: { required: false, validate: validatePassword },
+  });
+
+  const form = useForm<IForm>({
+    defaults: {
+      career: "",
+      level: undefined,
+      aim: "",
+    },
+    validators,
+    onSubmit: (data) => {
+      if (!user) return;
+      updateUser.mutate({
+        id: user.id,
+        payload: {},
+      });
+    },
+  });
+
+  return (
+    <Form onSubmit={form.onSubmit} className="w-full max-w-[448px]">
+      <div className="flex flex-col mx-auto gap-6">
+        <div className="flex flex-col gap-2 sm:gap-4">
+          <Input
+            id="job"
+            name="job"
+            idleDir="rtl"
+            value={form.state.job}
+            inputSize={"large"}
+            autoComplete="off"
+            onChange={(e) => form.set("name", e.target.value)}
+            label={intl("labels.name")}
+            placeholder={intl("labels.name.placeholder")}
+            state={form.errors.name ? "error" : undefined}
+            helper={form.errors.name}
+          />
+
+          <PatternInput
+            mask=" "
+            id="phone"
+            idleDir="ltr"
+            inputSize="large"
+            name="phone"
+            value={form.state.phone}
+            onValueChange={(e) => form.set("phone", e.value)}
+            format="### #### ####"
+            label={intl("labels.phone")}
+            placeholder={intl("labels.phone.placeholder")}
+            state={form.errors.phone ? "error" : undefined}
+            helper={form.errors.phone || intl("complete-profile.phone.helper")}
+            autoComplete="off"
+          />
+
+          <Select
+            id="city"
+            value={form.state.city}
+            onChange={(value) => form.set("city", value)}
+            options={cityOptions}
+            label={intl("labels.city")}
+            placeholder={intl("labels.city.placeholder")}
+            helper={form.errors.city || intl("complete-profile.city.helper")}
+          />
+
+          {!user?.password ? (
+            <Password
+              idleDir="rtl"
+              id="password"
+              name="password"
+              onChange={(e) => form.set("password", e.target.value)}
+              value={form.state.password}
+              inputSize="large"
+              label={intl("labels.password")}
+              state={form.errors.password ? "error" : undefined}
+              placeholder={intl("labels.create-password.placeholder")}
+              helper={form.errors.password}
+            />
+          ) : null}
+        </div>
+        <div className="flex gap-4 items-center">
+          <Button
+            type="main"
+            size="large"
+            htmlType="submit"
+            className="w-full text"
+            disabled={updateUser.isPending}
+            loading={updateUser.isPending}
+          >
+            {intl("labels.confirm")}
+          </Button>
+          <Button
+            type="main"
+            size="large"
+            onClick={goRoot}
+            htmlType="button"
+            className="w-full text"
+            variant="secondary"
+            disabled={updateUser.isPending}
+          >
+            {intl("labels.skip")}
+          </Button>
+        </div>
+      </div>
+    </Form>
+  );
+};
+
+export default Bio;
--- packages/ui/src/components/Stepper/Stepper.tsx
+++ packages/ui/src/components/Stepper/Stepper.tsx
@@ -37,3 +37,5 @@ export const Stepper: React.FC<{
     </ol>
   );
 };
+
+export default Stepper;
