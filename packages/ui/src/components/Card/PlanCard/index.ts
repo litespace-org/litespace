e594540291a6f82690ae11a62b1bbb6e8fe15059
Dev.Mo <muhdid82@gmail.com>
2025-08-25 17:25:04 +0300

    feat(web): redesign plans page

--- packages/ui/src/components/Card/PlanCard/index.ts
+++ packages/ui/src/components/Card/PlanCard/index.ts
@@ -1,4 +1,3 @@
-export { Card } from "@/components/Card/Card";
 export { PlanCard } from "@/components/Card/PlanCard/PlanCard";
 export { DiscountBadge } from "@/components/Card/PlanCard/DiscountBadge";
 export { FeaturesList } from "@/components/Card/PlanCard/FeaturesList";
