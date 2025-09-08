ab0565b17e3494a805034d8c71314461ef63b09b
M. E. Abdelsalam <mo.ehab150@gmail.com>
2025-09-08 17:36:11 +0300

    impl: students schema and associated model functions.

--- packages/types/src/student.ts
+++ packages/types/src/student.ts
@@ -1,4 +1,4 @@
-import { IUser } from ".";
+import { IFilter, IUser } from "@/index";
 
 export enum EnglishLevel {
   Beginner = 1,
@@ -21,7 +21,6 @@ export type Self = {
 
 export type Row = {
   id: number;
-  user_id: number;
   job_title: string | null;
   english_level: EnglishLevel | null;
   learning_objective: string | null;
@@ -37,3 +36,24 @@ export type UpdateApiResponse = unknown;
 
 export type Column = keyof Row;
 export type Field = keyof Self;
+
+export type CreateModelPayload = {
+  userId: number;
+  jobTitle?: string | null;
+  englishLevel?: EnglishLevel | null;
+  learningObjective?: string | null;
+};
+
+export type UpdateModelPayload = {
+  id: number;
+  jobTitle?: string | null;
+  englishLevel?: EnglishLevel | null;
+  learningObjective?: string | null;
+};
+
+export type FindModelQuery = IFilter.SkippablePagination & {
+  ids?: number[];
+  jobTitle?: string;
+  englishLevels?: EnglishLevel[];
+  learningObjective?: string;
+};
