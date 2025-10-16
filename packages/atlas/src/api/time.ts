21922e307908d3915075c682e991e8bf04ef1225
M. E. Abdelsalam <mo.ehab150@gmail.com>
2025-10-16 06:15:22 +0300

    enhance: check is valid time request.

--- packages/atlas/src/api/time.ts
+++ packages/atlas/src/api/time.ts
@@ -4,6 +4,15 @@ export class Time extends Base {
   async currentZoneTime(
     timezone: string
   ): Promise<{ iso: string; hour: number; minute: number; second: number }> {
-    return this.get({ route: `/api/v1/time/current`, params: { timezone } });
+    return this.client
+      .get("/api/v1/time/current", {
+        params: { timezone },
+        headers: {
+          "Cache-Control": "no-cache, no-store, must-revalidate",
+          Pragma: "no-cache",
+          Expires: "0",
+        },
+      })
+      .then((response) => response.data);
   }
 }
